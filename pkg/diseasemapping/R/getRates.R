4f1f119aa2435f4f88095a8d68c078b15d74d8e9
lutong <lutong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-09 21:35:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/getRates.R
+++ pkg/diseasemapping/R/getRates.R
@@ -5,10 +5,31 @@
 `getRates` <-
 function(casedata, popdata, formula, family=poisson, minimumAge=0,
    maximumAge=100, S=c("M", "F"),cyears=NULL,year.range=NULL,
-   case.years=grep("^year$", names(casedata), ignore.case=TRUE, value=TRUE)[1]){
+   case.years=grep("^year$", names(casedata), ignore.case=TRUE, value=TRUE)[1], breaks=NULL){
 # check the formula is one sided
+
+      thevarying = c(grep("^M.*_", names(popdata), value=TRUE), grep("^F.*_", names(popdata), value=TRUE))       
+      poplong = reshape(popdata,  varying=thevarying, direction="long", v.names="POPULATION", timevar="GROUP", times = thevarying)
+
+
+    if (length(breaks) > 0) {      
+    currentbreaks <- attributes(poplong)$breaks    
+         if (length(currentbreaks) == 0){ 
+              currentbreaks <- getBreaks(popdata)}
+         if (all(breaks %in% currentbreaks)) {
+        attributes(poplong)$breaks = breaks
+        } else {
+        stop("population age breaks aren't nested in the breaks provided")}
+    } else {breaks <- attributes(poplong)$breaks}
+    if (length(breaks) == 0){
+    breaks <- getBreaks(popdata)
+    attributes(poplong)$breaks <- breaks
+    }
+
+
 if(attributes(terms(formula))$response)
   warning("formula should be one sided")
+
 
 morethanoneyear = class(popdata)=="list"
 
@@ -101,7 +122,7 @@ newdata$YEAR= factor(newdata$YEAR, levels = unique(newdata$YEAR))
 
 newdata = newdata[newdata$POPULATION>0,]
 newdata$logpop = log(newdata$POPULATION)
-
+	
 
 # make the age group with the most cases as the base line
 agevar =  grep("^age$", theterms, ignore.case=TRUE)
@@ -122,4 +143,25 @@ model = glm(formula1, family=family, data=newdata)
 model$sexSubset = S
 
 return(model)
+}
+
+
+
+
+
+
+
+getBreaks <- function(colNames) {
+popColumns <- grep("^(m|f|male|female)[[:digit:]]+(_|-|plus|\\+)[[:digit:]]*$", colNames, value=T, ignore.case=T)
+ageGroups <- gsub("^(m|f|male|female)", "", popColumns, ignore.case=TRUE)
+ageGroups <- gsub("(\\+|plus)", "_Inf", ageGroups, ignore.case=TRUE)
+ageLower <- as.numeric(gsub("(_|-)([[:digit:]]+|Inf)$", "", ageGroups) )
+ageUpper <- as.numeric(gsub("^[[:digit:]]+(_|-)", "", ageGroups) )
+currentbreaks <- c(sort(unique(ageLower)), max(ageUpper))
+
+sex <- substr(popColumns,1,1)
+ageCut <- as.character(cut(ageLower, breaks=currentbreaks, right=F)) # I
+return(list(breaks=currentbreaks, names=ageCut, age=ageLower, sex=sex, 
+	oldNames=popColumns, 
+	newNames=paste(sex, ageLower, sep=".")))
 }
