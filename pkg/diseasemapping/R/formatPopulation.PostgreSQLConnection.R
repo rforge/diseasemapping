b1618f2a9d43079e19e752d641254fa00a42b13e
billy <billy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-28 04:33:01 +0000

    updated my PostgreSQL connection function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/formatPopulation.PostgreSQLConnection.R
+++ pkg/diseasemapping/R/formatPopulation.PostgreSQLConnection.R
@@ -1,20 +1,78 @@
-`formatPopulation.PostgreSQLConnection` <-
-function(popdata, aggregate.by = NULL, subset=NULL, ...) {
+formatPopulation.PostgreSQLConnection <- function(dbname,user="postgres",driver="PostgreSQL",aggregate=F) {
 
-# create sql command to transform data to long format
+  # function to get data from a database
+  # Input:
+  # dbname = name of database
+  # user = user name for the database, default='postgres'
+  # driver = database driver, default='PostgreSQL'
+  # aggregate = if TRUE, return aggregated population data by age and sex group
+  # Output:
+  # the raw data from the database, or the aggregated data if requested
 
+  # functionality very limited, don't think this can work on data except our current data
 
-# do the aggregation
-if(!is.null(aggregate)) {
+  # example
+  #result <- formatPopulation.PostgreSQLConnection("spatial")
+  #result.agg <- formatPopulation.PostgreSQLConnection("spatial",aggregate=T)
+
+  
+  require(RPostgreSQL)
+
+  # should get this from the database!
+  stuff = c("CSDUID","CSDNAME","CSDTYPE","PRUID","PRNAME","CDUID","CDNAME","CDTYPE","CMAUID","CMANAME","SACTYPE","ERUID","ERNAME","TotPop2001","TotPop2006","TotPopPerc","TotPop","AreaSqKm20","TotPop_x_A","MaleTot","FemaleTot")
+  
+  sql <- c()
+
+  begin <- T # start loop indicator
+
+  for (sex in c("M","F")) { # over Males and Females
+    
+    for (age in seq(0,80,by=5)) { # need to handle M85plus and F85plus!
+    
+      if (aggregate==T) {
+
+        # sum things up for each category
+        one <- paste('select sum("',sex,age,'_',age+4,'") as population,\'',sex,'\' as sex, \'',age,'_',age+4,'\' as age from "Ontario"."CSD2006"' ,sep="")
+
+        if (begin==T) { # turn loop start indicator off
+          sql <- one
+          begin <- F
+        }
+        else sql <- paste(sql,'union',one) # combine sql commands with 'union'
+        
+      }
+
+      else {
+    # get the columns
+        one <-   paste('select',paste(c('"',paste(stuff,collapse='","'),'",'),collapse=''))
+
+    # add sex and age columns
+        two <-   paste(c('"',sex,age,'_',age+4,'" as population, \'',sex,age,'_',age+4,'\' as group, \'',sex,'\' as sex, \'',age,'_',age+4,'\' as age from "Ontario"."CSD2006"'),collapse="")
+
+        if (begin==T) { # turn loop start indicator off
+          sql <- paste(one,two)
+          begin <- F
+        }
+        else sql <- paste(sql,'union',one,two) # combine sql commands with 'union'
+        
+      }
+    }
+  }
+
+  # send sql query to database
+  drv <- dbDriver(driver)
+  con <- dbConnect(drv,dbname=dbname,user=user)
+  fs <- dbSendQuery(con,sql)
+  fetch(fs,n=-1)
 
 }
 
 
 
-# some R code to add age and sex variables
+#----------- for reference ------------#
 
-
-# return the aggregated data
-
-
-}
+#select 'M0_4', "CSDUID", "CSDNAME", "CSDTYPE", "PRUID", "M0_4" as population, 'M' as sex, 0 as age from "Ontario"."CSD2006"
+#union 
+#select 'M5_9', "CSDUID", "CSDNAME", "CSDTYPE", "PRUID", "M5_9" as population, 'M' as sex, 5 as age from "Ontario"."CSD2006"
+#union
+#select 'M10_14', "CSDUID", "CSDNAME", "CSDTYPE", "PRUID", "M10_14" as population, 'M' as sex, 10 as age from "Ontario"."CSD2006";
