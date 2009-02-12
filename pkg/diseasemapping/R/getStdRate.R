44b80cf22d193e54da75a79299337ecc4eefd3c8
lutong <lutong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-12 15:55:58 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/getStdRate.R
+++ pkg/diseasemapping/R/getStdRate.R
@@ -1,5 +1,71 @@
-getStdRate= function(relativeRate, model, referencePopulation, scale=100000) {
+# the function body:
+getStdRate= function(relativeRate, model, referencePopulation="Canada1991",
+  scale=100000) {
 
-relativeRate * sum(predict(model, referencePopulation)) * scale
+# check for negatives
+if(any(relativeRate < 0 ))
+  warning("negative numbers in rates, make sure they're not on the log scale")
 
+if(is.character(referencePopulation)) {
+  data(referencePopulations)
+  referencePopulation = referencePopulations[[referencePopulation]]
+
+}
+  
+newpop <- formatCases(referencePopulation)
+
+newpop <- newpop[newpop$POPULATION!=0,]
+
+newpop$logpop <- log(newpop$POPULATION) + log(scale)
+
+# predict.glmZeros
+
+# find the ages used in the model and get rid of those that do not exist in model
+modelAges = model$xlevels$age
+newpop= newpop[newpop$age %in% modelAges,]
+#remove main effects that not in the model
+for(Dlevel in names(model$xlevels)) {
+alllevels = levels(newpop[[Dlevel]])
+if(!all(alllevels %in% model$xlevels[[Dlevel]])) {
+	# remove the rows from the missing levels
+	tokeep = newpop[[Dlevel]] %in% model$xlevels[[Dlevel]]
+	newpop = newpop[tokeep,]
+ }
+}
+
+
+#Take out interactions with NA, work with model without interactions
+#temp<-row.names(summary(model)$coefficients)[-1]
+interactNA<-names(model$coefficients)[is.na(model$coefficients)]
+#if there is any coeff with NA, remove
+if(length(interactNA)>0){
+  interact<-grep(":",interactNA,value=TRUE)
+  #keep the rows that the interaction is not NA,
+  newpop$param<-paste(paste("age",newpop$age,sep=""),paste("sex",newpop$sex,sep=""),sep=":")
+  newpop = newpop[!newpop$param %in% interact,]
+  newpop$param<-NULL
+}
+#find expected
+
+#agg<-c("age","sex","logpop")
+
+
+newpop$pred <- predict(model, newpop[,c("age","sex","logpop")], type = "response")
+
+#referenceRate <- sum(predict(model, newpop[,c("age","sex","logpop")], type = "response"))
+
+return(relativeRate*referenceRate)
+
+#names(newpop)<-c("age","sex", "pred")
+
+for(i in 1:length(relativeRate)){
+newpop[,paste("Region",i,sep="")] = newpop$pred * relativeRate[i]
+}
+newpop
+}
+
+
+
+getstdRate.list()<-function(relativeRate, model, referencePopulation, scale=100000){
+lapply(referencePopulation,getstdRate,relativeRate=relativeRate, scale=scale, model=model)
 }
