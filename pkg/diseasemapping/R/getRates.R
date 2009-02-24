d34a736d50217c6b7b7abf8dfd3c150dccf0da63
lutong <lutong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-24 19:04:20 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/getRates.R
+++ pkg/diseasemapping/R/getRates.R
@@ -107,7 +107,8 @@ if(length(agevar)==1) {
   newdata[[agevar]] = factor(as.character(newdata[[agevar]]),
     levels= agetable)
 }
-
+sexvar = grep("^sex", theterms, ignore.case=TRUE)
+newdata[[sexvar]] = factor(newdata[[sexvar]])
 
 # add cases and logpop to formula
 formula1 = update.formula(formula, CASES ~ offset(logpop) + .)
@@ -129,17 +130,3 @@ return(model)
 
 
 
-getBreaks <- function(colNames) {
-popColumns <- grep("^(m|f|male|female)[[:digit:]]+(_|-|plus|\\+)[[:digit:]]*$", colNames, value=T, ignore.case=T)
-ageGroups <- gsub("^(m|f|male|female)", "", popColumns, ignore.case=TRUE)
-ageGroups <- gsub("(\\+|plus)", "_Inf", ageGroups, ignore.case=TRUE)
-ageLower <- as.numeric(gsub("(_|-)([[:digit:]]+|Inf)$", "", ageGroups) )
-ageUpper <- as.numeric(gsub("^[[:digit:]]+(_|-)", "", ageGroups) )
-currentbreaks <- c(sort(unique(ageLower)), max(ageUpper))
-
-sex <- substr(popColumns,1,1)
-ageCut <- as.character(cut(ageLower, breaks=currentbreaks, right=F)) # I
-return(list(breaks=currentbreaks, names=ageCut, age=ageLower, sex=sex, 
-	oldNames=popColumns, 
-	newNames=paste(sex, ageLower, sep=".")))
-}
