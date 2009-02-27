8707bc6a7ef601b7686cd0d328170acb021c3333
lutong <lutong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-27 17:57:37 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/formatCases.R
+++ pkg/diseasemapping/R/formatCases.R
@@ -6,46 +6,49 @@ haveAgeSex = length(grep("^age$", names(casedata), ignore.cases=T)) &
 
 # if not, is there an age_sex_group column, in rif format?  use it to create age and sex
 groupvar = grep("^AGE_SEX_GROUP$", names(casedata), value=TRUE, ignore.case=TRUE)
- 
+agecol = grep("^age$", names(casedata), value=TRUE, ignore.case=TRUE)
+sexcol = grep("^sex$", names(casedata), value=TRUE, ignore.case=TRUE)
+
+
 if(length(groupvar)){
-# create age and sex columns
-if(length(grep("_", casedata[[groupvar]], ignore.case=TRUE ))){ 
-casedata$sex = substr(casedata[[groupvar]], 1, 1)
-ss= getBreaks(casedata[[groupvar]])
-casedata$age = ss$age
-#casedata$group =ss$newNames
+ if(length(grep("_", casedata[[groupvar]], ignore.case=TRUE ))){
+# if sex column is missing, creat it
+if(!length(sexcol)) {
+  casedata$sex = substr(casedata[[groupvar]], 1, 1)
+  sexcol = "sex"
+}
+# if age column is missing, creat it
+if(!length(agecol)){
+  n <- regexpr("_", casedata[[groupvar]], fixed = TRUE)
+  casedata$age = substr(casedata[[groupvar]], n-2, n-1)
+  agecol = "age"
+}
 }else{
-        casedata$sex = factor(substring(casedata[[groupvar]], 1, 1), levels=c("1", "2"), labels=c("M", "F"))
-        casedata$age = 5*as.numeric(substring(casedata[[groupvar]], 2, 3))
-#        casedata$group = paste(casedata$sex, casedata$age, sep=".")
-}     
+# if groupvar is in rif format:
+if(!length(sexcol)) {
+  casedata$sex = factor(substring(casedata$AGE_SEX_GROUP, 1, 1), levels=c("1", "2"), labels=c("M", "F"))
+  sexcol = "sex"
+}
+if(!length(agecol)){
+  casedata$age = 5*as.numeric(substring(casedata[[groupvar]], 2, 3))
+  agecol = "age"
+}
+}
 }
 
-# are there age and sex columns?
-if(!length(groupvar)){
-haveSex = grep("sex", names(casedata), value=TRUE, ignore.case=TRUE)
-haveAge = grep("age", names(casedata), value=TRUE, ignore.case=TRUE)
-      if(length(haveSex)){casedata$sex = substr(casedata[[haveSex]], 1, 1)}
-      if(length(haveAge)){
-             if(length(grep("_", casedata[[haveAge]], ignore.case=TRUE))) {casedata$age = substr(casedata[[haveAge]], 1, 2)}
-                   }
-#             casedata$groupd = paste(casedata$sex, casedata$age, sep=".")      
-                   }
-                  
-## if there are underscores in age, take the number before the underscores
-##if(there are underscores) {
-##casedata$age = as.numeric(grep("(_[[:digit:]]+$|PLUS$)", "", casedata$age))
-##}
-
-## if not, is there an age_sex_group column, in rif format?  use it to create age and sex
-
-# use the cut function on age
-
-if(!is.null(ageBreaks)) {
- casedata$cutAge = as.character(cut(as.numeric(as.character(casedata$age)), ageBreaks$breaks, right=F))
+if(!is.null(ageBreaks)){
+  casedata$ageNumeric = casedata[[agecol]]
+ casedata$age = as.character(cut(as.numeric(as.character(casedata$ageNumeric)),
+    ageBreaks$breaks, right=F))
+  attributes(casedata)$breaks = ageBreaks
+}else{
+    casedata$ageNumeric = casedata[[agecol]]
+    casedata$age =  as.character(cut(as.numeric(as.character(casedata$ageNumeric)),
+    sort(as.numeric(unique(casedata[[agecol]]))), right=F))
+    attributes(casedata)$breaks = ageBreaks
 }
 
- # aggregate, if necessary
+# aggregate, if necessary
 if(!is.null(aggregate.by)) {
    popa = aggregate(casedata$cases, list(age=casedata$cutAge), sum)
    names(popa)[names(popa)=="x"] = "CASES"
@@ -55,3 +58,7 @@ if(!is.null(aggregate.by)) {
 attributes(casedata)$breaks = ageBreaks
 casedata
 }
+<<<<<<< .mine
+
+=======
+>>>>>>> .r63
