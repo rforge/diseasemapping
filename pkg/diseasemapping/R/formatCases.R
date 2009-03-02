8301eb35f2d117216e2fcae2182fe4e3f8bdbc4f
lutong <lutong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-02 18:25:52 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/formatCases.R
+++ pkg/diseasemapping/R/formatCases.R
@@ -1,40 +1,46 @@
 `formatCases` <- function(casedata, ageBreaks=NULL, years=NULL, aggregate.by=NULL) {
 
 # are there age and sex columns?
-haveAgeSex = length(grep("^age$", names(casedata), ignore.cases=T)) &
-    length(grep("^sex$", names(casedata), ignore.cases=T)) &
-
-# if not, is there an age_sex_group column, in rif format?  use it to create age and sex
-groupvar = grep("^AGE_SEX_GROUP$", names(casedata), value=TRUE, ignore.case=TRUE)
+#haveAge = grep("^age$", names(casedata), value=TRUE, ignore.case=T)
+#haveSex = grep("^sex$", names(casedata), value=TRUE, ignore.case=T)
 agecol = grep("^age$", names(casedata), value=TRUE, ignore.case=TRUE)
 sexcol = grep("^sex$", names(casedata), value=TRUE, ignore.case=TRUE)
 
-
-if(length(groupvar)){
- if(length(grep("_", casedata[[groupvar]], ignore.case=TRUE ))){
-# if sex column is missing, creat it
-if(!length(sexcol)) {
-  casedata$sex = substr(casedata[[groupvar]], 1, 1)
-  sexcol = "sex"
-}
-# if age column is missing, creat it
-if(!length(agecol)){
-  n <- regexpr("_", casedata[[groupvar]], fixed = TRUE)
-  casedata$age = substr(casedata[[groupvar]], n-2, n-1)
-  agecol = "age"
-}
+if(length(agecol) & length(sexcol)){
+casedata$age = casedata[[agecol]]
+casedata$sex = casedata[[sexcol]]
 }else{
+# if not, is there an age_sex_group column, in rif format?  use it to create age and sex
+groupvar = grep("^AGE_SEX_GROUP$", names(casedata), value=TRUE, ignore.case=TRUE)
+#agecol = grep("^age$", names(casedata), value=TRUE, ignore.case=TRUE)
+#sexcol = grep("^sex$", names(casedata), value=TRUE, ignore.case=TRUE)
+if(length(groupvar)){
+    if(length(grep("_", casedata[[groupvar]], ignore.case=TRUE ))){
+# if sex column is missing, creat it
+        if(!length(sexcol)) {
+            casedata$sex = substr(casedata[[groupvar]], 1, 1)
+            sexcol = "sex"
+            }
+# if age column is missing, creat it
+        if(!length(agecol)){
+            n <- regexpr("_", casedata[[groupvar]], fixed = TRUE)
+            casedata$age = substr(casedata[[groupvar]], n-2, n-1)
+            agecol = "age"
+            }
+            }else{
 # if groupvar is in rif format:
-if(!length(sexcol)) {
-  casedata$sex = factor(substring(casedata$AGE_SEX_GROUP, 1, 1), levels=c("1", "2"), labels=c("M", "F"))
-  sexcol = "sex"
+       if(!length(sexcol)) {
+            casedata$sex = factor(substring(casedata$AGE_SEX_GROUP, 1, 1), levels=c("1", "2"), labels=c("M", "F"))
+            sexcol = "sex"
+            }
+       if(!length(agecol)){
+            casedata$age = 5*as.numeric(substring(casedata[[groupvar]], 2, 3))
+            agecol = "age"
+            }
+            }
 }
-if(!length(agecol)){
-  casedata$age = 5*as.numeric(substring(casedata[[groupvar]], 2, 3))
-  agecol = "age"
 }
-}
-}
+
 
 if(!is.null(ageBreaks)){
   casedata$ageNumeric = casedata[[agecol]]
@@ -43,8 +49,7 @@ if(!is.null(ageBreaks)){
   attributes(casedata)$breaks = ageBreaks
 }else{
     casedata$ageNumeric = casedata[[agecol]]
-    casedata$age =  as.character(cut(as.numeric(as.character(casedata$ageNumeric)),
-    sort(as.numeric(unique(casedata[[agecol]]))), right=F))
+    casedata$age =  as.character(cut(as.numeric(as.character(casedata$ageNumeric)), sort(as.numeric(unique(casedata[[agecol]]))), right=F))
     attributes(casedata)$breaks = ageBreaks
 }
 
@@ -58,7 +63,4 @@ if(!is.null(aggregate.by)) {
 attributes(casedata)$breaks = ageBreaks
 casedata
 }
-<<<<<<< .mine
 
-=======
->>>>>>> .r63
