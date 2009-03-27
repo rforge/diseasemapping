9d46fbe71b8f22ba13efb3e87423cd311f41d17f
yeli <yeli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-27 20:48:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@151 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/rasterSMR.list.r
+++ pkg/diseasemapping/R/rasterSMR.list.r
@@ -1,4 +1,7 @@
-rasterSMR.list<-function(poplist,bbox=NULL,nrows=200, ncols=200, xmn=NULL, xmx=NULL, ymn=NULL, ymx=NULL, projs="NA",columns=c("expected")){
+#addCensus indicates if adding the values specified in columns argument over census
+#return a list of rasters stacks if addCensus is False, otherwise a single stack
+rasterSMR.list<-function(poplist,bbox=NULL,xmn=NULL, xmx=NULL, ymn=NULL, ymx=NULL,
+        cellFine=c(50,50), cellCoarse=NULL, fact=NULL,projs="NA",columns=c("expected"),addCensus=TRUE){
 
 
 #find a bbox for all census
@@ -13,7 +16,21 @@ xlimits = c(min(x),max(x))
 ylimits = c(min(y),max(y))
 bbox<-extent(matrix(c(xlimits,ylimits),nrow=2,byrow=T))
 
-lapply(poplist,rasterSMR,bbox=bbox,nrows=nrows, ncols=ncols, xmn=xmn, xmx=xmx, ymn=ymn, ymx=ymx, projs=projs,columns=columns)
+a<-lapply(poplist,rasterSMR,bbox=bbox,fact=fact,cellFine=cellFine, cellCoarse=cellCoarse,xmn=xmn, xmx=xmx, ymn=ymn, ymx=ymx, projs=projs,columns=columns)
 
+if(addCensus){
+
+    #add up columns in each census, create a single stack
+    for(j in 1:length(columns)){
+      news<-a[[1]]@layers[[j]] #initialize
+      for (i in 2:length(a)){
+        news<-overlay(news,a[[i]]@layers[[j]],fun="+")
+      }
+     if(j==1){sta<-stack(news)}else{sta<-addRasters(sta,news)}
+    }
+
+  a<-sta
+}
+a
 
 }
