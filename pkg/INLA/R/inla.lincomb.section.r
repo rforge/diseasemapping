f0f85ee6f298561c20122ca567d759ae01e85d44
yeli <yeli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-07 14:05:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@194 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/INLA/R/inla.lincomb.section.r
+++ pkg/INLA/R/inla.lincomb.section.r
@@ -3,21 +3,24 @@
 
      file = paste(dir,"Model.ini",sep="/")
     ############Modify ini file
+    
     cat(paste("[Lincomb",num,"]\n",sep=""), sep = " ", file = file, append = TRUE)
     cat("type = lincomb\n", sep = " ", file = file, append = TRUE)
+    cat(paste("dir","=","fixed.effect","9999",num,"\n",sep=""),file = file, append = TRUE)
     cat("filename = ", paste("$DATADIR/lincomb",num,".dat",sep=""), "\n", sep = " ", file = file,append = TRUE)
     if (!is.null(lincomb.spec)) {
         cat("precision = ", lincomb.spec, sep = " ", file = file,
             append = TRUE)
         cat("\n", sep = " ", file = file, append = TRUE)
     }
-
+   cat("\n", sep = " ", file = file, append = TRUE)
 
     ############Write Data files
 
     ddir<-paste(dir, "data.files/",sep="/")
     datafile<-paste(ddir,paste("lincomb",num,".dat",sep=""),sep="/")
     
+     
 
       cat(struct, sep = " ", file = datafile)
         cat("\n", sep = " ", file = datafile, append = TRUE)
