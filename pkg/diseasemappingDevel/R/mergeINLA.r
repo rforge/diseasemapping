807de05028bd810f7cb4cc556ca3f4aaa6b4954c
yeli <yeli@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-09 01:10:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@202 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/diseasemappingDevel/R/mergeINLA.r
@@ -0,0 +1,35 @@
+mergeINLA<-function(data,INLA,spatial="region.struct",iid="region",exceed=1.2){
+
+  data$spatial<-INLA$summary.random[[spatial]][,2]
+  data$iid<-INLA$summary.random[[iid]][,2]
+  data$fitted<-INLA$summary.fitted.values[,1]
+  data$linear<-INLA$summary.fitted.values[,1]
+
+  #Get Joint
+  temp<-data.frame(bym=INLA$summary.fixed[-1,1],ID=as.integer(substr(names(temp),8,100))+1)
+  t<-temp[order(temp$ID),]
+  data$bym<-t$bym
+
+
+  #use predictos on the log scale to find out exceeding
+  data$fitted_exceed<-getEx(INLA$marginals.linear.predictor,exceed)
+  data$bym_exceed<-getEx(INLA$marginals.fixed[-1],exceed)
+
+
+  data
+}
+
+
+#Computer exceedance proabilities
+getEx<-function(post,r){
+        V<-NULL
+	for(i in 1:length(post)){
+
+		post1<-post[[i]]
+		colnames(post1) = c("x", "y")
+		V[i]<-sum(post1[post1[,"x"] > log(r), "y"])* unique(diff(post1[,"x"]))[1]
+	}
+       V
+
+}
+
