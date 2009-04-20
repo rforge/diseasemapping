e188cd046c80a17cca25813f63c636b1b76b824f
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-20 14:31:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@232 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/diseasemappingDevel/R/writeBivariateModel.R
@@ -0,0 +1,19 @@
+
+writeBivariateModel <- function() { # need to write in the arguments!
+
+
+# write smoking model
+writeBugsModel()
+
+# write cancer model
+
+writeBugsModel()
+
+
+# write the Vbig stuff
+
+# ?????
+
+
+
+}
