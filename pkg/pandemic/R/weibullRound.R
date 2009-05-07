f62480e92ee73c2fe839a2b9413b426f78a1369b
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-07 19:04:32 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@268 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/pandemic/R/weibullRound.R
@@ -0,0 +1,13 @@
+rweibullRound = function(N, params=c(shape=1, scale=1)) {
+  round(rweibull(N, shape=params[["shape"]], scale=params[["scale"]]))
+}
+
+
+dweibullRound = function(x, params=c(shape=1, scale=1)) {
+  xLow = pmax(x-0.5, 0)
+  xHigh = x+0.5
+  pweibull(xHigh, shape=params["shape"], scale=params["scale"]) - 
+    pweibull(xLow, shape=params["shape"], scale=params["scale"])
+                                                                   
+
+}
--- pkg/pandemic/test/test.R
+++ pkg/pandemic/test/test.R
@@ -4,3 +4,8 @@ source("../R/simEpidemic.R")
 params = pandemicParams()
  data = simEpidemic(params, 10)
  summary(data)
+
+ xseq=0:20
+
+plot(xseq, dweibullRound(xseq, params$HospRec))
+sum( dweibullRound(0:100, params$HospRec))
