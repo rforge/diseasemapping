51e23d562a318747476271a0ff82f60fde6e1e90
mcbsspn <mcbsspn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-11 14:12:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@287 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pandemic/R/weibullRound.R
+++ pkg/pandemic/R/weibullRound.R
@@ -13,6 +13,31 @@ dweibullRound = function(x, params=c(shape=1, scale=1)) {
 
 }
 
+
+
+dweibullzero= function(x, params=c(shape=1, scale=1,zeros=0)) {
+  xLow = pmax(x-0.5, 0)
+  xHigh = x+0.5
+ prob= pweibull(xHigh, shape=params["shape"], scale=params["scale"]) - 
+    pweibull(xLow, shape=params["shape"], scale=params["scale"])
+ prob=(1-params["zeros"])*prob
+  if(x==0) prob=params["zeros"]+prob                                                                  
+ prob
+}
+
 pweibullRound=function(x, params) {
   pweibull(x+0.5, shape=params["shape"], scale=params["scale"])
+}
+
+sumWeibull=function(k,params1,params2)
+{
+x=0
+for(j in 0:k)
+{
+for(i in 0:(k-j))
+{
+x=x+dweibullRound(j,params1)*dweibullRound(i,params2)
+}
+}
+x
 }
