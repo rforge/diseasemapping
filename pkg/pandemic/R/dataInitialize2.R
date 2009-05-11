30bc6c4c7888850fcb98e4b128e1b148d488d875
mcbsspn <mcbsspn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-11 13:23:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@285 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/pandemic/R/dataInitialize2.R
@@ -0,0 +1,141 @@
+censorweibull=function(a,b,cen, params)
+{
+weibull=0
+for(i in 1:length(cen))
+{
+weibull[i]=0
+while(weibull[i]<=cen[i])
+{
+weibull[i]=round(rweibull(1,a,b))
+}
+}
+weibull
+}
+
+
+
+
+# hosps
+inHosp = which(data$observedType == "hosp")
+inHospTimes = data[inHosp, "censor"] - data[inHosp, "hospital"]
+probCensorGivenDeadly = 1-pweibullRound(inHospTimes, params$HospDeath)
+probCensorGivenSerious = 1-pweibullRound(inHospTimes, params$HospRec)
+
+probDeadlyGivenCensor = probCensorGivenDeadly *params$probs["D"]
+
+probDeadlyGivenCensor = probDeadlyGivenCensor / 
+  (probDeadlyGivenCensor + probCensorGivenSerious)
+  
+data[inHosp,"type"] = c("S","D")[1+rbinom(length(inHosp), 1, probDeadlyGivenCensor)]  
+
+
+# meds
+inMed = which(data$observedType == "med")
+inMedTimes = data[inMed, "censor"] - data[inMed, "med"]
+
+probOf = data.frame(
+  "L" = 1* params$probs["M"] * params$MedRec["lost"], 
+  "M"= (1-pweibullRound(inMedTimes, params$MedRec)) *
+      (params$probs["M"] * (1- params$MedRec["lost"]) ) ,
+  "S"= params$probs["S"]*
+    (1-pweibullRound(inMedTimes, params$MedHospS)),
+  "D"= params$probs["D"]*
+    (1-pweibullRound(inMedTimes, params$MedHospD) )
+  )
+#sumProb = apply(probOf, 1, sum)  
+
+#probOf = apply(probOf, 2, function(qq) qq/sumProb)
+# generate states
+states = apply(probOf, 1, function(qq) {
+    sample(colnames(probOf), 1, prob=qq)
+} )
+
+theLost = states=="L" 
+states[theLost]="M"
+data$lost = FALSE
+data[inMed[theLost],"lost"] = T
+data[inMed,"type"] = states
+
+
+
+
+
+
+  needOnset=is.na(data$onset)
+
+  data[needOnset,"onset"]  = data[needOnset,"med"] - round(rweibull(sum(needOnset),
+    shape= getVecParams(params, "OnsMed", "shape")[as.character(data$type)],
+    scale= getVecParams(params, "OnsMed", "scale")[as.character(data$type)]
+    ) )   
+
+  needInfect=is.na(data$infect)
+
+  data[needInfect,"infect"]  = data[needInfect,"onset"]- round(rweibull(sum(needInfect),
+    shape= getVecParams(params, "InfOns", "shape"),
+    scale= getVecParams(params, "InfOns", "scale")
+    ) ) 
+
+ 
+   needhospital=((as.character(data$observedType)=="med")&(as.character(data$type)=="S"))
+
+   data[needhospital,"hospital"]=data[needhospital,"med"]+censorweibull(
+    getVecParams(params, "MedHospS", "shape"),
+    getVecParams(params, "MedHospS", "scale"),
+(data[needhospital,"censor"]-data[needhospital,"med"])
+    ) 
+
+# Edited up to here.
+   
+  data[needhospital,"removed"]  =  round(rweibull(sum(needhospital),
+  shape= getVecParams(params, "HospRec", "shape"),
+    scale= getVecParams(params, "HospRec", "scale")
+    ) )   
+
+   needhospital=((as.character(data$observedType)=="med")&(as.character(data$type)=="D"))
+
+   data[needhospital,"hospital"]=censorweibull(
+     getVecParams(params, "MedHospD", "shape"),
+    getVecParams(params, "MedHospD", "scale"),
+(data[needhospital,"censor"]-data[needhospital,"med"])
+    ) 
+
+   
+  data[needhospital,"removed"]  =  round(rweibull(sum(needhospital),
+  shape= getVecParams(params, "HospDeath", "shape"),
+    scale= getVecParams(params, "HospDeath", "scale")
+    ) )   
+
+
+   needremoved=((as.character(data$observedType)=="med")&(as.character(data$type)=="M"))
+   # depends if you're lost or not
+
+   # if lost
+   data[data$lost,"removed"]=round(rweibull(sum(data$lost),
+  shape= getVecParams(params, "MedRec", "shape"),
+    scale= getVecParams(params, "MedRec", "scale")
+    ) )   
+    #if not lost
+
+
+
+   needremoved=((as.character(data$observedType)=="hosp")&(as.character(data$type)=="S"))
+
+   data[needremoved,"removed"]=censorweibull(
+getVecParams(params, "HospRec", "shape"),
+ getVecParams(params, "HospRec", "scale"),
+(data[needremoved,"censor"]-data[needremoved,"hospital"]-data[needremoved,"med"])
+    ) 
+
+   needremoved=((as.character(data$observedType)=="hosp")&(as.character(data$type)=="D"))
+
+   data[needremoved,"removed"]=censorweibull(
+getVecParams(params, "HospDeath", "shape"),
+ getVecParams(params, "HospDeath", "scale"),
+(data[needremoved,"censor"]-data[needremoved,"hospital"]-data[needremoved,"med"])
+    ) 
+
+data
+
+
+
+
---
+++ pkg/pandemic/R/simEpidemic2.R
@@ -0,0 +1,136 @@
+simEpidemic <- function(params, delta=2, days=50,
+  probOnsetMissing=0.7) {
+  
+  if(!any(names(params[["OnsMedM"]])=="scale"))
+    warning("can't find scale parameter, use addScaleParameters")
+  
+  infectionsPerDay=rpois(days,delta)
+
+  N=sum(infectionsPerDay)
+
+  colNames <- c("infect", "onset",  "med", "hospital",
+     "removed", "censor", "type","observedType")
+  result <- matrix(NA, N, length(colNames), 
+    dimnames = list(NULL, colNames))
+  result <- as.data.frame(result)
+  
+  j=0
+for(i in 1:days)
+{
+if(infectionsPerDay[i]>0)
+{
+result$infect[(j+1):(j+infectionsPerDay[i])]=i
+j=j+infectionsPerDay[i]
+}
+}
+#  result
+
+#}
+
+#simEpidemic(params)
+
+
+  theTypes = c("M","S","D")
+  result$type = sample(factor(theTypes, levels=theTypes),
+    N, replace=T, prob=params$probs)
+  
+  theTypes = c(theTypes, "med","hosp")
+  result$observedType = factor(result$type, levels=theTypes)
+  
+  result$onset=result[,"infect"]+round(rweibull(N,
+    shape= getVecParams(params, "InfOns", "shape"),
+    scale= getVecParams(params, "InfOns", "scale")
+    ) )   
+  
+  result$med=result[,"onset"]+round(rweibull(N,
+    shape= getVecParams(params, "OnsMed", "shape")[as.character(result$type)],
+    scale= getVecParams(params, "OnsMed", "scale")[as.character(result$type)]
+    ) )   
+
+N=sum(result$med<=days)
+
+result=result[result$med<=days,]
+
+  # onset times
+  haveOnset = as.logical(rbinom(N, 1, 1-probOnsetMissing))
+  
+  result[haveOnset==F,"onset"]  = NA
+  result[,"infect"]=NA
+
+  # mild infections, 
+  # recovery date
+  theMild = which(result$type=="M")
+  Nmild = length(theMild)
+  # lost to followup
+  theLost = theMild[as.logical(rbinom(Nmild, 1, params[["MedRec"]]["lost"]))]
+  result[theLost,c("type", "removed")] = NA
+  result[theLost,"censor"] = days
+  result[theLost,"observedType"] = "med"
+
+  # if not lost, generate recovery times
+
+  Nmild = sum(result$observedType=="M")
+  
+     result[result$observedType=="M","removed"]=result[result$observedType=="M","med"]+round(rweibull(Nmild,
+    shape= getVecParams(params, "MedRec", "shape"),
+    scale= getVecParams(params, "MedRec", "scale")
+    ) )   
+  
+
+  for(i in 1:N)
+  {
+  if((result[i,"observedType"]=="M")&(result[i,"removed"]>days))
+  {
+  result[i,"type"]=NA
+  result[i,"observedType"]="med"
+  result[i,"removed"]=NA
+  result[i,"censor"]=days  
+  }
+  } 
+
+
+  # serious and deadly infection
+  # hospitalization
+  theSerious = result$type=="S"
+  theDeadly = result$type=="D"
+  theNA = is.na(result$type)
+  theSerious[theNA] = theDeadly[theNA] = FALSE
+  
+  result[theSerious, "hospital"] = result[theSerious,"med"]+
+    rweibullRound(sum(theSerious), params[["MedHospS"]])
+  result[theDeadly, "hospital"] = result[theDeadly,"med"]+
+    rweibullRound(sum(theDeadly), params[["MedHospD"]])
+
+  
+  censored = (result$hospital) > days
+  censored[is.na(censored)] = FALSE
+  result[censored,c("type", "hospital")] <- NA
+  result[censored,"censor"] <- days
+  result[censored,"observedType"] <- "med"
+
+  
+
+  # recovery or death
+  inHosp = !is.na(result$hospital)
+  seriousHosp = theSerious & inHosp
+  deadlyHosp = theDeadly & inHosp
+  result[seriousHosp,"removed"] = result[seriousHosp,"hospital"]+ 
+     rweibullRound(sum(seriousHosp), params[["HospRec"]])
+
+  result[deadlyHosp,"removed"] = result[deadlyHosp,"hospital"]+ 
+     rweibullRound(sum(deadlyHosp), params[["HospDeath"]])
+
+  censored = inHosp & ((result$removed > days) )
+  censored[is.na(censored)] = FALSE
+  result[censored,c("type","removed")] <- NA
+  result[censored,"censor"] <- days
+  result[censored,"observedType"] <- "hosp"
+ 
+  result=result[order(result$med),]
+
+  result
+  
+}
+
+simEpidemic(params)
+
