0d659f56bda97386cc28cd1448a999c3d3cba5a8
pnguyen <pnguyen@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-06 19:29:36 +0000

    update area function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@481 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/computeArea.r
+++ pkg/diseasemapping/R/computeArea.r
@@ -1,4 +1,4 @@
 #return a list of areas
 `computeArea`<-function(sp){
-	lapply(sp@polygons, function(x) sum(sapply(x@Polygons, function(y) y@area * ifelse(y@hole, -1, 1))))
+	lapply(sp@polygons, function(x) sum(sapply(x@Polygons, function(y) y@area * ifelse(y@hole, 0, 1))))
 }
