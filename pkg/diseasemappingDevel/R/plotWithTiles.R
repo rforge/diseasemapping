4c833f756aa4a50456dc30cb18282e11a09ecb91
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-13 20:00:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@484 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemappingDevel/R/plotWithTiles.R
+++ pkg/diseasemappingDevel/R/plotWithTiles.R
@@ -14,29 +14,40 @@ plotWithTiles = function(x, attr = 1, brks = NULL, prob = FALSE,
 		tilepath = "http://a.www.toolserver.org/tiles/osm-no-labels/"
 	}
 	
-	if(prob == TRUE)
-	{
-		brks <- c(0,0.2,0.8,0.95,1)
-		colours <- c("#00FF00","#FFFF00","#FF6600","#FF0000")
-	} else{
-		library(RColorBrewer)
-		colours <- brewer.pal(Ncol, "YlOrRd")
-	}
-
-	if(is.null(brks))
-	{
+	
+	if(is.null(brks) & !is.null(attr))
+	{		
 		ci <- classInt::classIntervals(x@data[,attr], Ncol, style = "kmeans")
 		brks <- ci$brks * 100
 		brks <- c(floor(brks[1]), round(brks[-c(1,length(brks))]), ceiling(brks[length(brks)]))/100
 	}
-
+	if(is.character(Ncol)){
+		colours=Ncol
+	} else {
+		library(RColorBrewer)
+		colours <- brewer.pal(Ncol, "YlOrRd")
+	}
+	
+	if(prob == TRUE)
+	{
+		brks <- c(0,0.2,0.8,0.95,1)
+		colours <- c("#00FF00","#FFFF00","#FF6600","#FF0000")
+	} 
+	
 	if(class(x) %in% c("SpatialGridDataFrame","SpatialPixelsDataFrame"))
 	{
 		library(raster)
 		x <- raster(x, layer = attr)
+
+	} 
+	if (! class(x) %in% c("raster", "SpatialPointsDataFrame")  ){
+		stop("x must be raster or SpatialGridDataFrame or SpatialPixelsDataFrame or SpatialPointsDataFrame object")
+	}
+
+	if(class(x)=="raster") {
 		x <- projectRaster(x, crs = "+proj=longlat +datum=NAD83")
 	} else {
-		stop("x must be SpatialGridDataFrame or SpatialPixelsDataFrame object")
+		x = spTransform(x, CRS("+proj=longlat +datum=NAD83"))
 	}
 	
 	if(is.null(xlim))
@@ -58,19 +69,22 @@ plotWithTiles = function(x, attr = 1, brks = NULL, prob = FALSE,
 		}	
 	}
 	
-	plot(1, type = "n", xlim = xlim,ylim=ylim, xlab=NA,ylab=NA,axes=F)
+	plot(x,  xlim = xlim,ylim=ylim, xlab=NA,ylab=NA,axes=F)
 
 	image(bgMap, add = TRUE)
 	
 	brkstemp <- brks
 	brkstemp[length(brkstemp)] <- brkstemp[length(brkstemp)] + 0.01
+	if(!is.null(attr)) {
 	plot(x, breaks = brkstemp, col = paste(colours, substring(hsv(alpha = as.numeric(trans)/100),8), sep = ""), 
 		legend = FALSE, horizontal = FALSE, add = TRUE)
 
 	legend("bottomright", fill = colours, cex = label, 
 		legend = c(paste("[", brks[1:(length(brks) - 2)], ",", brks[-c(1,length(brks))], ")", sep = ""), 
 			paste("[", brks[length(brks) - 1], ",", brks[length(brks)], "]", sep = "")), bg = "white")
-	
+	} else {
+		plot(x, add=T, col=colours)		
+	}
 
 	if(!is.null(filename) & devOff)
 	{
