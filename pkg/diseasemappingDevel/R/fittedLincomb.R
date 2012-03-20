4c03eed25116440744cc73bf4a68f675bcab6d90
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-20 20:19:14 +0000

    problem with environments in fittedLincomb
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@487 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemappingDevel/R/fittedLincomb.R
+++ pkg/diseasemappingDevel/R/fittedLincomb.R
@@ -3,11 +3,12 @@ fittedLincomb = function(inlaResult){
 	allterms = attributes(terms(inlaResult$formula))$term.labels
 	theterms = allterms[-grep("^f\\(", allterms)]
 	theRE = names(inlaResult$summary.random)
-	if(length(theRE)>1)
+	if(length(theRE)!=1)
 		warning("more than one random effect", theRE)
 	
-	theUnique = which(!duplicated(inlaResult[[theRE]]))
+	theUnique = which(!duplicated(inlaResult$data[[theRE]]))
 	forCellID = matrix(NA, dim(inlaResult$summary.random[[theRE]])[1],length(theUnique))
+	
 	
 	for(D in seq(1, length(theUnique))) {
     	forCellID[inlaResult$data[theUnique[D],theRE],D] = 1
@@ -17,9 +18,18 @@ fittedLincomb = function(inlaResult){
 	forLincomb = list(cellID=forCellID, "(Intercept)"=rep(1,length(theUnique)))
 	names(forLincomb)[1] = theRE
 	
-	theLincomb = inla.make.lincombs(inla.uncbind(as.matrix(
-							inlaResult$data[theUnique,theterms])), 
-			forLincomb)
+	#return(list(theUnique, theterms, forLincomb,inla.uncbind(as.matrix(
+	#								inlaResult$data[theUnique,theterms]))))
+	
+	theMatrix = as.matrix(inlaResult$data[theUnique,theterms])
+	theMatrix2 = inla.uncbind(theMatrix)
+	
+	assign("theMatrix2QQ", theMatrix2,pos=1)
+	
+	
+	theLincomb = inla.make.lincombs(theMatrix2QQ, forLincomb)
+	
+	rm(theMatrix2QQ, pos=1)
 	
 	theLincomb
 	
