b47694a3c5c5dab4ebf708a122a00f35d4ec8d0a
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-06-18 16:36:38 +0000

    fixed expectedes per unit area
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@540 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/getSMR.SpatialPolygonsDataFrame.R
+++ pkg/diseasemapping/R/getSMR.SpatialPolygonsDataFrame.R
@@ -1,5 +1,18 @@
-`getSMR.SpatialPolygonsDataFrame` <- function(popdata, ...) {
+`getSMR.SpatialPolygonsDataFrame` <- function(popdata, model, casedata=NULL, 
+		regionCode = intersect(names(popdata), names(casedata))[1],
+		regionCodeCases=regionCode, area=FALSE,  ...) {
 
-popdata@data <- getSMR(popdata@data, ...)
+	
+	if (area & !("sqk" %in% names(popdata) ) ) {
+		if(length(grep("longlat", popdata@proj4string@projargs)))
+			warning("computing areas of polygons in long-lat projection")
+		popdata$sqk = sapply(slot(popdata, "polygons"), slot, "area")
+	}	
+	
+popdata@data <- getSMR(popdata@data, model, casedata, regionCode,
+		regionCodeCases, area,...)
+
+
+
 popdata
 }
