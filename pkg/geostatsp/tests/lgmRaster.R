3264cc2787feb8f0c47d8a9a2df43ff5b8ddf610
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-06 20:12:12 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@870 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/geostatsp/tests/lgmRaster.R
+++ pkg/geostatsp/tests/lgmRaster.R
@@ -41,3 +41,26 @@ swissResR$summary[ 'range' ,] * sqrt(mean(values(area(swissRainR))))/mean(res(sw
 # profile likelihood plot
 
 # plot map of predicted values
+
+
+# a simulation
+if(FALSE) {
+	
+	myModel = c(intercept=0,variance=2^2,nugget=0.5^2, range=2.5,shape=2, 
+			cov1=0.2, cov2=-0.5)
+	covariates = brick(
+			xmn=0,ymn=0,xmx=10,ymx=10,
+			ncols=200,nrows=200,nl=2)
+	values(covariates)[,1] = rep(seq(0,1,len=nrow(covariates)), ncol(covariates))
+	values(covariates)[,2] = rep(seq(0,1,len=nrow(covariates)), 
+			rep(nrow(covariates), ncol(covariates)))
+	names(covariates) = c("cov1","cov2")
+	myU = RFsimulate(model=myModel,x=raster(covariates), n=5)
+	myMean = myModel["intercept"] 
+	for(D in names(covariates))
+		myMean = myMean + covariates[[D]]*myModel[D]
+	myY = myU
+	for(D in names(myY)) {
+		myY[[D]] = myY[[D]] + myMean
+	}	
+}
