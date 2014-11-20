3dbbb046314e80b04122356bb0d1ca717e5b2118
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-20 17:55:24 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@910 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tmp/stuff.R
+++ tmp/stuff.R
@@ -40,3 +40,79 @@ debug(glgm)
 			shape=1,  fixShape=TRUE, 
 			boxcox=0.5, fixBoxcox=TRUE, 
 			aniso=TRUE)	
+	
+	
+	list(mal_data)
+	
+	names(mal)
+	
+	library(mapmisc)
+	malmap = openmap(mal)
+
+	map.new(mal)
+	plot(malmap,add=TRUE)
+	plot(mal,add=TRUE)
+	
+	
+#altitude
+	map.new(mal)
+	plot(malmap,add=TRUE)
+	plot(ALT, col=terrain.colors(100), main="elevation", alpha=0.5, add=TRUE)
+	points(mal)
+	
+	
+#NDVI
+map.new(mal)
+plot(malmap,add=TRUE)
+	plot(NDVI, main="mean ndvi",alpha=0.5,col=terrain.colors(100), add=TRUE)
+	points(mal)
+	
+#Population
+map.new(mal)
+plot(malmap,add=TRUE)
+plot(POP, main="Population Density",alpha=0.5,col=terrain.colors(100), add=TRUE)
+	points(mal)
+	
+	
+	
+#Prepare data for modeling
+	covList = list(alt=ALT, maxtempwmq=MAXTWMM, precwetmn=PRECWTM,
+			precdrm=PRECDRM, #precwetq=PRECWQ, 
+			preccoldq=PRECCQ, 
+			ndvi=NDVI, pop=POP)
+	
+for(D in names(covList)) {
+	map.new(mal)
+	plot(malmap,add=TRUE)
+	plot(POP, alpha=0.5,col=terrain.colors(100), add=TRUE)
+	points(mal, col='#FF000070')
+	mtext(D,side=3, adj=0.8,line=-3,cex=2)
+	invisible(readline(prompt="Press [enter] to continue"))	
+}	
+
+
+
+
+
+#Run the model
+	malFit<-glgm(
+			formula = POS ~ alt + maxtempwmq + precwetmn + 
+					precdrm + #precwetq + 
+					preccoldq + ndvi + pop, 
+			data = mal, grid = 50,
+			covariates = covList, family = "binomial", 
+			Ntrials = mal$TOT, shape = 1,buffer = 50000, 
+			priorCI = list(sd = c(0.2, 4), range = c(20000, 5e+05)))
+	
+	grid=50;data=mal;buffer=50000;covariates=covList
+	grid=gridRaster = squareRaster(data, grid)
+	cellsBoth = cellsBuffer(grid, buffer)			
+	cellsSmall = cellsBoth$small
+	
+	rmethod = rep("bilinear", length(names(covariates)))
+	names(rmethod) = names(covariates)
+	rmethod['precwetq'] = "ngb"
+	covariatesStack = stackRasterList(covariates, 
+			template=cellsSmall, 
+			method=rmethod)
+	
