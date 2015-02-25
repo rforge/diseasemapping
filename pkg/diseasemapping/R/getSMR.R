271e452918eded252466ef66cb75ea7b2ac811e1
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-25 02:50:21 +0000

    new methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1028 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/getSMR.R
+++ pkg/diseasemapping/R/getSMR.R
@@ -1,206 +1,252 @@
+aggPopLong = function(popdata, poplong, sex, area.scale) {
+
+# called by getSMR df numeric and getSMR df glm  
+  
+if('sex' %in% names(poplong)){
+  sex = paste("^", paste(sex, collapse='$|^'), "$", sep='')
+  poplong= poplong[grep(sex, poplong$sex, ignore.case=TRUE),]
+}	
+
+poplong <- aggregate(poplong$expected, 
+    list(poplong[['idForAgg']]), 
+    sum, na.rm=TRUE)
+rownames(poplong) = as.character(poplong[,1])
+poplong=poplong[poplong[,2] > 0,]
+
+
+# merge results back in to the population data
+# the merge function changes the order, so can't use it.
+popdata$expected = NA
+rownames(popdata) = as.character(popdata[,'idForAgg'])
+
+popdata[rownames(poplong), "expected"] = poplong[,2]
+
+surfaceArea = grep("^surfacearea$", names(popdata), ignore.case=TRUE, value=TRUE)
+if (length(surfaceArea) ) {
+  popdata$expected_surfaceArea <- area.scale *popdata$expected/popdata[[surfaceArea[1] ]]
+  popdata$logExpected_surfaceArea = log(popdata$expected_surfaceArea)
+}
+
+popdata$logExpected = log(popdata$expected)
+
+popdata = popdata[,
+    grep('^idForAgg$', colnames(popdata), invert=TRUE)]
+
+
+popdata
+
+}
+
 setGeneric('getSMR', 
-    function(popdata, model, casedata=NULL, 
-    regionCode = intersect(names(popdata), names(casedata))[1],
-    regionCodeCases=regionCode, area=FALSE,  ...) {
+    function(popdata, model, casedata, 
+        regionCode,
+        regionCodeCases, 
+        area.scale=1, 
+        sex=c('m','f'), ...
+) {
       standardGeneric("getSMR")
     }
 )
 
+
+
+
 setMethod("getSMR", 
-    signature("SpatialPolygonsDataFrame"),
-    function(
-    popdata, model, casedata=NULL, 
-		regionCode = intersect(names(popdata), names(casedata))[1],
-		regionCodeCases=regionCode, area=FALSE,  ...) {
+    signature("data.frame", "numeric", "missing", "ANY"),
+    function(popdata, model, 
+        casedata, 
+        regionCode,
+        area.scale=1, 
+        sex=c('m','f'), ...){
+      # model is a vector of rates
+      
+      popdata$idForAgg = 1:nrow(popdata)
+ 
+      # check breaks for groups, make sure they line up
+      rateBreaks = getBreaks(names(model))
+      popBreaks = getBreaks(names(popdata))
+      
+      newBreaks = getBreaks(intersect(rateBreaks$newNames, popBreaks$newNames))
+      
+      newModel = data.frame(age=rateBreaks$age, sex=rateBreaks$sex, 
+          rate=model)
+      newModel = formatCases(newModel, newBreaks)
+      newModel = aggregate(newModel$rate, 
+          newModel[,c('age','sex')],mean,na.rm=T)
+      rownames(newModel) = 
+          paste(newModel$sex, 
+              newModel$age, 
+              sep='.')
+      
+      poplong = formatPopulation(popdata, breaks=newBreaks$breaks)
+      
+      poplong$expected = poplong$POPULATION *  
+          newModel[paste(poplong$sex, poplong$age, sep='.')
+              ,'x']
 
-	
-	if (area & !("surfaceArea" %in% names(popdata) ) ) {
-		if(length(grep("longlat", popdata@proj4string@projargs)))
-			warning("computing areas of polygons in long-lat projection")
-		popdata$surfaceArea = sapply(slot(popdata, "polygons"), slot, "area")
-	}	
-	
-popdata@data <- getSMR(popdata@data, model, casedata, regionCode,
-		regionCodeCases, area,...)
-
-
-
-popdata
-}
+      aggPopLong(popdata, poplong, sex, area.scale) 
+    }
 )
 
 setMethod("getSMR", 
-    signature("data.frame"),
-    function(popdata, model, casedata=NULL, 
-    regionCode = intersect(names(popdata), names(casedata))[1],
-    regionCodeCases=regionCode, area=FALSE, area.scale=1, ...){
-  
-  
-  if(!length(regionCode)) {
-    
-    regionCode='id'
-    popdata$id = 1:nrow(popdata)
-    
-  }
-  
-  if(is.numeric(model)) {
-    # model is a vector of rates
-    # check breaks for groups, make sure they line up
-    # popdata = kpop2[,-c(8:19,31:42)]
-    rateBreaks =getBreaks(names(model))
-    popBreaks = getBreaks(names(popdata))
-    
-    newBreaks = getBreaks(intersect(rateBreaks$newNames, popBreaks$newNames))
-    
-    newModel = data.frame(age=rateBreaks$age, sex=rateBreaks$sex, 
-        rate=model)
-    newModel = formatCases(newModel, newBreaks)
-    newModel = aggregate(newModel$rate, 
-        newModel[,c('age','sex')],mean,na.rm=T)
-    rownames(newModel) = 
-        paste(newModel$sex, 
-            newModel$age, 
-            sep='.')
-    
-    poplong = formatPopulation(popdata, breaks=newBreaks$breaks)
-    
-    poplong$expected = poplong$POPULATION *  
-        newModel[paste(poplong$sex, poplong$age, sep='.')
-            ,'x']
-    
-    if(any(names(list(...))=='sex')){
-      sex=list(...)$sex
-      if('sex' %in% names(poplong)){
-        poplong= poplong[poplong$sex %in% toupper(sex),]
-      }	
-      if(!is.null(casedata)) {
-        if('sex' %in% colnames(casedata)){
-          casedata = casedata[
-              toupper(casedata[,'sex']) %in% toupper(sex), 
-          ]
-        }
+    signature("data.frame", "glm", "missing", "ANY"),
+    function(popdata, model, casedata, 
+        regionCode,
+        area.scale=1, 
+        sex=c('m','f'), ...){
+      
+      popdata$id = 1:nrow(popdata)
+      poplong <- formatPopulation(popdata, breaks=attributes(model)$breaks$breaks)
+      
+      p<-grep("^population$", names(poplong), value=TRUE, ignore.case=TRUE)  
+      
+      poplong[is.na(poplong[,p]),p] <- 0
+      
+      popBreaks = attributes(poplong)$breaks
+      
+      # get rid of zero populations,because they dont lead to rates of exactly zero
+      ## check the class of the POPULATION column and make sure it is numeric: 
+      # converted to character first in case it's a factor
+      # (convert factor names to integers, not factor levels)
+      
+      if(class(poplong[,p]) != "numeric"){
+        poplong[,p] <- as.numeric(as.character(poplong[,p]))
       }
-    }
-  } else {
-    # use the predict method on the model
-    
-    poplong <- formatPopulation(popdata, breaks=attributes(model)$breaks$breaks)
-    
-    p<-grep("^population$", names(poplong), value=TRUE, ignore.case=TRUE)  
-    
-    
-    poplong[is.na(poplong[,p]),p] <- 0
-    
-    popBreaks = attributes(poplong)$breaks
-    
-    # get rid of zero populations,because they dont lead to rates of exactly zero
-    ## check the class of the POPULATION column and make sure it is numeric: 
-    # converted to character first in case it's a factor
-    # (convert factor names to integers, not factor levels)
-    
-    if(class(poplong[,p]) != "numeric"){
-      poplong[,p] <- as.numeric(as.character(poplong[,p]))
-    }
-    
-    poplong=poplong[poplong[,
-            grep("^population$", names(poplong), value=TRUE, ignore.case=TRUE)]>0, ]     
-    #changes poplong names to be consistent with model
-    agevar<-grep("^age$",names(attributes((terms(model)))$dataClasses),value=TRUE,ignore.case=TRUE)
-    sexvar<-grep("^sex$",names(attributes((terms(model)))$dataClasses),value=TRUE,ignore.case=TRUE)
-    yearvar<-grep("^year$",names(attributes((terms(model)))$dataClasses),value=TRUE,ignore.case=TRUE)
-    
-    agevar1<-grep("^age$",names(poplong),value=TRUE,ignore.case=TRUE)
-    sexvar1<-grep("^sex$",names(poplong),value=TRUE,ignore.case=TRUE)
-    yearvar1<-grep("^year$",names(poplong),value=TRUE,ignore.case=TRUE)
-    
-    if(length(agevar) & length(agevar1)){
-      names(poplong[[agevar1]])=agevar
-    }
-    if(length(sexvar) & length(sexvar1)){
-      names(poplong[[sexvar1]])=sexvar
-    }
-    if(length(yearvar) & length(yearvar1)){
-      names(poplong[[yearvar1]])=yearvar
-    }
-    
-    # get rid of a gender
-    if (length(model$sexSubset) == 1) {
-      poplong = poplong[poplong[[sexvar1]] == model$sexSubset, ]
-    }
-    
-    
-    offsetvar<- grep("^offset",names(attributes((terms(model)))$dataClasses)  ,value=TRUE, ignore.case=TRUE)
-    offsetvar<- substr(offsetvar,8,nchar(offsetvar)-1)
-    
-    
-    poplong[[offsetvar]] = log(poplong$POPULATION)
+      
+      poplong=poplong[poplong[,
+              grep("^population$", names(poplong), value=TRUE, ignore.case=TRUE)]>0, ]     
+      #changes poplong names to be consistent with model
+      agevar<-grep("^age$",names(attributes((terms(model)))$dataClasses),value=TRUE,ignore.case=TRUE)
+      sexvar<-grep("^sex$",names(attributes((terms(model)))$dataClasses),value=TRUE,ignore.case=TRUE)
+      yearvar<-grep("^year$",names(attributes((terms(model)))$dataClasses),value=TRUE,ignore.case=TRUE)
+      
+      agevar1<-grep("^age$",names(poplong),value=TRUE,ignore.case=TRUE)
+      sexvar1<-grep("^sex$",names(poplong),value=TRUE,ignore.case=TRUE)
+      yearvar1<-grep("^year$",names(poplong),value=TRUE,ignore.case=TRUE)
+      
+      if(length(agevar) & length(agevar1)){
+        names(poplong[[agevar1]])=agevar
+      }
+      if(length(sexvar) & length(sexvar1)){
+        names(poplong[[sexvar1]])=sexvar
+      }
+      if(length(yearvar) & length(yearvar1)){
+        names(poplong[[yearvar1]])=yearvar
+      }
+      
+      # get rid of a gender
+      if (length(model$sexSubset) == 1) {
+        poplong = poplong[poplong[[sexvar1]] == model$sexSubset, ]
+      }
+      
+      
+      offsetvar<- grep("^offset",names(attributes((terms(model)))$dataClasses)  ,value=TRUE, ignore.case=TRUE)
+      offsetvar<- substr(offsetvar,8,nchar(offsetvar)-1)
+      
+      
+      poplong[[offsetvar]] = log(poplong$POPULATION)
 #    poplong[[sexvar]]= factor(poplong[[sexvar]])
 #    poplong[[agevar]] = factor(poplong[[agevar]])
-    #names(poplong) <- tolower(names(poplong))
-    for (Dlevel in names(model$xlevels)) {
-      alllevels = levels(poplong[[Dlevel]])
-      if (!all(alllevels %in% model$xlevels[[Dlevel]])) {
-        tokeep = poplong[[Dlevel]] %in% model$xlevels[[Dlevel]]
-        poplong = poplong[tokeep, ]
+      #names(poplong) <- tolower(names(poplong))
+      for (Dlevel in names(model$xlevels)) {
+        alllevels = levels(poplong[[Dlevel]])
+        if (!all(alllevels %in% model$xlevels[[Dlevel]])) {
+          tokeep = poplong[[Dlevel]] %in% model$xlevels[[Dlevel]]
+          poplong = poplong[tokeep, ]
+        }
       }
+      interactNA <- names(model$coefficients)[is.na(model$coefficients)]
+      if (length(interactNA) > 0) {
+        interact <- grep(":", interactNA, value = TRUE)
+        poplong$param <- paste(paste("age", poplong$age, sep = ""),
+            paste("sex", poplong$sex, sep = ""), sep = ":")
+        poplong = poplong[!poplong$param %in% interact, ]
+        poplong$param <- NULL
+      }
+      if(length(yearvar1)) {
+        agg<-c(yearvar1,agevar, sexvar,offsetvar)
+      }else{
+        agg<-c(agevar, sexvar, offsetvar)
+      }
+      
+      # multiply population by popScale, to make it in person years
+      if(any(names(attributes(popdata))=="popScale")) {
+        poplong[,offsetvar]=     poplong[,offsetvar] + log(attributes(popdata)$popScale)
+      }
+      
+      
+      poplong$expected <- predict(model, poplong,#[,agg],
+          type = "response")
+      
+      aggPopLong(popdata, poplong, sex, area.scale) 
+
     }
-    interactNA <- names(model$coefficients)[is.na(model$coefficients)]
-    if (length(interactNA) > 0) {
-      interact <- grep(":", interactNA, value = TRUE)
-      poplong$param <- paste(paste("age", poplong$age, sep = ""),
-          paste("sex", poplong$sex, sep = ""), sep = ":")
-      poplong = poplong[!poplong$param %in% interact, ]
-      poplong$param <- NULL
+)
+
+# set regionCode and regionCodeCases with case data
+setMethod("getSMR", 
+    signature("data.frame", "ANY", "data.frame", 'missing', 'missing'),
+    function(popdata, model, casedata, 
+        regionCode,
+        regionCodeCases, area.scale=1, 
+        sex=c('m','f'), ...
+    ){
+      
+      regionCodeCases=regionCode = intersect(names(popdata), 
+          names(casedata)
+      )[1]
+      
+      
+      if(!length(regionCodeCases))
+        warning('cant find region code in case data')
+      
+      callGeneric()
+      
     }
-    if(length(yearvar1)) {
-      agg<-c(yearvar1,agevar, sexvar,offsetvar)
-    }else{
-      agg<-c(agevar, sexvar, offsetvar)
+)
+
+# set regionCode and regionCodeCases with case data
+setMethod("getSMR", 
+    signature("data.frame", "ANY", "data.frame", 'character', 'missing'),
+    function(popdata, model, casedata, 
+        regionCode,
+        regionCodeCases, area.scale=1, 
+        sex=c('m','f'), ...
+    ){
+      
+      regionCodeCases=regionCode
+      
+      
+      if(!length(grep(regionCodeCases, names(casedata))))
+        warning('cant find region code in case data')
+      
+      callGeneric(popdata, model, casedata, regionCode,
+          regionCodeCases, area.scale, sex)
+      
     }
-    
-    # multiply population by popScale, to make it in person years
-    if(any(names(attributes(popdata))=="popScale")) {
-      poplong[,offsetvar]=     poplong[,offsetvar] + log(attributes(popdata)$popScale)
-    }
-    
-    
-    poplong$expected <- predict(model, poplong,#[,agg],
-        type = "response")
-  } # done predicting rates from model
-  
-  poplong <- aggregate(poplong$expected, list(poplong[[regionCode]]), 
-      sum, na.rm=TRUE)
-  rownames(poplong) = as.character(poplong[,1])
-  poplong=poplong[poplong[,2] > 0,]
-  
-  
-  # merge results back in to the population data
-  # the merge function changes the order, so can't use it.
-  popdata$expected = NA
-  rownames(popdata) = as.character(popdata[,regionCode])
-  
-  popdata[rownames(poplong), "expected"] = poplong[,2]
-  
-  
-  
-  if (area & ("surfaceArea" %in% names(popdata) ) ) {
-    popdata$expected_surfaceArea <- area.scale *popdata$expected/popdata$surfaceArea
-    popdata$logExpected_surfaceArea = log(popdata$expected_surfaceArea)
-    
-  }
-  
-  
-  popdata$logExpected = log(popdata$expected)
-  popdata$observed = 0
-  # change NA's and -Inf in logExpected to zeros, so that it can be used in models
-  popdata$logExpected[is.na(popdata$logExpected)] = 0
-  popdata$logExpected[popdata$logExpected==-Inf] = 0
-  
-  
-  if (!is.null(casedata)) {
-    
-# find column with cases
-    casecol = grep("^cases$|^count$|^y$", names(casedata), value=TRUE, ignore.case=TRUE)
+)
+
+
+setMethod("getSMR", 
+    signature("data.frame", "ANY", "data.frame", "character", "character"),
+    function(popdata, model, casedata, 
+        regionCode,
+        regionCodeCases, area.scale=1, 
+        sex=c('m','f'), ...
+){
+# have case data, compute SMR
+
+    popdata = callGeneric(
+        popdata, model,
+        area.scale=area.scale,
+        sex=sex
+        )
+      
+        if(!length(regionCode)) {
+          regionCode='id'
+        }
+
+        casecol = grep("^cases$|^count$|^y$", names(casedata), value=TRUE, ignore.case=TRUE)
     if(length(casecol)>1) {
       casecol=casecol[1]
       warning("more than one column which could be interpreted as case numbers, using ", casecol)
@@ -212,33 +258,69 @@ setMethod("getSMR",
       casedata[,casecol] = 1
     }
     
+    
     casedata = casedata[
         as.character(casedata[, regionCodeCases]) %in% 
-            rownames(popdata), ]
+            as.character(popdata[[regionCode]]), ]
     
     casedata <- aggregate(casedata[,casecol], 
         list(casedata[,regionCodeCases]), sum)
     names(casedata) = c(regionCodeCases, "observed")
     
-    
-    popdata[as.character(casedata[,1]),"observed"] = casedata[,2]
+    popdata[['observed']] = casedata[
+        pmatch(
+        as.character(popdata[[regionCode]]),
+        as.character(casedata[[regionCodeCases]])
+        ),
+        'observed']
     
     # change 0's in expected to NA, so SMR is NA
     theexpected = popdata$expected
     theexpected[theexpected==0] = NA
     
     popdata$SMR <- popdata$observed/theexpected
-  }
-  
-  popdata
-}
+
+    popdata
+    }
 )
 
 
 setMethod("getSMR", 
-    signature("list"),
-function(popdata, model, casedata = NULL, regionCode,
-    regionCodeCases, area, area.scale , 
+    signature("SpatialPolygonsDataFrame", 'ANY','ANY', "ANY"),
+    function(
+        popdata, model,  casedata, 
+        regionCode,
+        regionCodeCases, area.scale=1, 
+        sex=c('m','f'), ...
+    ) {
+      
+      area = length(grep("longlat", popdata@proj4string@projargs))
+      
+      if (area & !("surfaceArea" %in% names(popdata) ) ) {
+        popdata$surfaceArea = sapply(slot(popdata, "polygons"), slot, "area")
+      }	
+
+      popdata$idForGetSmr = 1:length(popdata)
+      theSP = popdata
+      popdata = data.frame(popdata)
+      
+      forSp <- callGeneric()
+
+      theSP@data = forSp[pmatch(
+              theSP$idForGetSmr,
+              forSp$idForGetSmr
+              ),]
+      theSP = theSP[,grep("^idForGetSmr$", names(theSP), invert=TRUE)]    
+      theSP
+    }
+)
+
+
+
+setMethod("getSMR", 
+    signature("list", 'ANY', 'ANY'),
+function(popdata, model, casedata, regionCode,
+    regionCodeCases, area.scale , 
     years = NULL, personYears=TRUE,year.range = NULL,...){
 #  lennon's stuff
   #isSP = (class(popdata[[1]]) == "SpatialPolygonsDataFrame")
