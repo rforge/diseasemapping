dc4c8e684994d6bc9bd8093e972a031ffb57a1de
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-01-04 20:13:42 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1302 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/geostatsp/R/glgm.R
+++ pkg/geostatsp/R/glgm.R
@@ -511,7 +511,7 @@ inlaResult$summary.random[['space']][,"exp"] = temp
 	inlaResult$summary.lincomb.derived[,"exp"] = temp
 	
 	# E inv logit(lincombs)
-	if(length(grep("binomial|beta",inlaResult$.args$family))) {
+	if(length(grep("logit",inlaResult$linkfunctions$names))) {
 		temp=unlist(
 				lapply(inlaResult$marginals.lincomb.derived, function(qq) {
 							eqqx = exp(qq[,"x"])
