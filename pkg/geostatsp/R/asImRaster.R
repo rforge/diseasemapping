8591e9824c16b2aceea99a4ed6e55467f313a5d0
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-03-21 21:49:13 +0000

    rasters for as.im
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1341 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/geostatsp/R/asImRaster.R
+++ pkg/geostatsp/R/asImRaster.R
@@ -4,9 +4,21 @@
 
 asImRaster = function(X, ...) {
   if(requireNamespace('spatstat',quietly=TRUE)){	
-	res=spatstat::as.im(raster::as.matrix(X)[nrow(X):1,], 
+	
+	if(nlayers(X)>1) X = X[[1]]	
+	xMat = raster::as.matrix(X)[nrow(X):1,]
+	if(is.factor(X)) {
+		facTable = levels(X)[[1]]
+		xMat = matrix(
+				facTable[match(xMat, facTable[,1]), min(c(2,ncol(facTable)))], 
+				nrow(xMat), ncol(xMat))
+		levels(xMat) = facTable[,min(2, ncol(facTable))]
+	}
+		
+	res=spatstat::as.im(xMat, 
 			xrange=bbox(X)[1,], 
 			yrange=bbox(X)[2,], ...)
+	
 } else {
 	message("Install the spatstat package if you wish to use this function.")
 	res = NULL
