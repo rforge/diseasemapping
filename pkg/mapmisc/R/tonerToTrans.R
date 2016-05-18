577f2b95c10be34c8264616355d56c9cef6fc3c3
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-18 19:46:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1384 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mapmisc/R/tonerToTrans.R
+++ pkg/mapmisc/R/tonerToTrans.R
@@ -1,12 +1,20 @@
 
 tonerToTrans = function(x, power=0.5, col='black') {
-	if(!any(names(x)=='stamen.tonerRed'))
-		warning("x doesn't seem to be stamen-toner")
-
-	newTrans = ((255-values(x[['stamen.tonerRed']]))/255)^power
+	
+	rgbLayers = grep("(red|green|blue)$", 
+			names(x), 
+			ignore.case=TRUE)
+	
+	if(length(rgbLayers != 3))
+		warning("x doesn't seem to be RGB")
+	
+	xMax = calc(x[[rgbLayers]], 
+			min, na.rm=FALSE)
+	xMaxValue = max(maxValue(x))
+	newTrans = ((xMaxValue-values(xMax))/xMaxValue)^power
 	newTrans[is.na(newTrans)] = 0
 	
-	col = col2rgb(col)[,1]
+	col = col2rgb(col)[,1]/255
 	
 	newCol = grDevices::rgb(
 			col['red'],
@@ -19,6 +27,50 @@ tonerToTrans = function(x, power=0.5, col='black') {
 	result = raster(x)
 	names(result) = 'stamen.toner'
 	values(result) = as.numeric(newCol)
+	result@legend@colortable = c(NA,levels(newCol))
+	result
+}
+
+
+rgbtToIndex = function(x) {
+	
+	rgbLayers = grep("(red|green|blue|trans)$", 
+			names(x), 
+			ignore.case=TRUE)
+	
+
+	if(length(rgbLayers != 4))
+		warning("x doesn't seem to be RGB trans")
+	
+	xsub = subs(
+			x[[rgbLayers]], 
+			data.frame(NA,0), 
+			subsWithNA=FALSE)
+	
+
+	xMaxValue = max(maxValue(x), na.rm=TRUE)
+
+	newCol = grDevices::rgb(
+			values(xsub[[grep("red$", names(xsub), ignore.case=TRUE)[1]]]),
+			values(xsub[[grep("green$", names(xsub), ignore.case=TRUE)[1]]]),
+			values(xsub[[grep("blue$", names(xsub), ignore.case=TRUE)[1]]]),
+			values(xsub[[grep("trans$", names(xsub), ignore.case=TRUE)[1]]]),
+			maxColorValue = xMaxValue
+			)
+	newCol = factor(newCol)		
+	
+	newCol[grep("00$", newCol)] = NA
+	
+	newCol = factor(newCol)
+
+	result = raster(x)
+
+	names(result) = gsub(
+			"(red|green|blue|trans)$", "", names(x), 
+			ignore.case=TRUE)[1]
+
+	values(result) = as.numeric(newCol)
+
 	result@legend@colortable = c(NA,levels(newCol))
 	result
 }
