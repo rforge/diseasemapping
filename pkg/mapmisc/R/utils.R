d7b128fbdbaef398c3ac677f2ea1dd2584c13f98
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-19 02:28:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1389 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mapmisc/R/utils.R
+++ pkg/mapmisc/R/utils.R
@@ -1,10 +1,9 @@
 # crsLL = CRS("+epsg:4326")
 crsLL = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0") 
 
-#  crsMerc =CRS("+init=epsg:3857") # mercator projection
-crsMercLL = CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +ellps=WGS84 +units=m +nadgrids=@null +no_defs")
-
-crsMercSphere = crsMerc = CRS("+proj=merc +ellps=sphere +units=m")
+#crsMercSphere = crsMerc = CRS("+proj=merc +ellps=sphere +units=m")
+crsMercSphere = crsMerc = 
+		CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +no_defs")
 
 crsSphere = CRS("+proj=longlat +ellps=sphere")
 
