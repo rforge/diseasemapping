5c358606817c2f5377992f7ba3b8a0fb4480f602
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-08-31 18:38:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1498 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Pmisc/R/make.R
+++ pkg/Pmisc/R/make.R
@@ -1,12 +1,3 @@
-#' Run make
-#' 
-#' @description Runs make with the Makefile supplied by Pmisc
-#'
-#' @param x a string specifying the target
-#' @param suffix string to replace file extension
-#' @param beamer use \code{--to=beamer}
-#' @param run execute the command
-#' @param ... additional arguments passed to make
 #' @export
 make = function(x, 
 		suffix=NULL, beamer=FALSE,
@@ -36,4 +27,69 @@ if(run) {
 
 invisible(theString)
 
+}
+
+#' @export
+Makefile = function(x, suffix = NULL,
+		beamer = FALSE, output='Makefile') {
+
+if(!is.null(suffix))
+		x = paste(
+				tools::file_path_sans_ext(basename(x)),
+				suffix, sep='.')
+	
+	
+before = list(
+	pandocTo = c('beamer', 'latex')[2-beamer],
+	docxTemplate = system.file('src/template.docx', package='Pmisc'),
+ odtTemplate = system.file('src/template.odt', package='Pmisc')
+)
+
+before = paste(paste(names(before), before, sep='='), collapse='\n')
+before = paste(before, '\n\nall: ', x, '\n\n')
+
+result = paste(
+		before,
+'.PRECIOUS: %.md %.tex
+
+%.md: %.Rmd
+	R -e "knitr::knit(\'$<\', encoding=\'UTF-8\')" $(Rargs)
+
+%.tex: %.md
+	pandoc --standalone --smart --biblatex $(pandocArgs) --to=$(pandocTo) --output=$@ $<
+
+%.bcf: %.tex
+	xelatex $<
+
+%.bbl: %.bcf
+	biber $<	
+
+%.pdf: %.tex %.bbl
+	xelatex -interaction=nonstopmode $<;
+	xelatex -interaction=nonstopmode $<
+
+%.html: %.md
+	pandoc --smart --standalone --mathjax --filter=pandoc-citeproc $(pandocArgs) --to=html5 --output=$@ $<
+
+%.rtf: %.md
+	pandoc --smart --standalone --filter=pandoc-citeproc $(pandocArgs) --output=$@ $<
+
+%.odt: %.md
+	pandoc --smart --standalone --filter=pandoc-citeproc $(pandocArgs) --reference-docx=$(odtTemplate) --output=$@ $<
+
+%.docx: %.md
+	pandoc --smart --standalone --filter=pandoc-citeproc $(pandocArgs) --reference-docx=$(docxTemplate) --output=$@ $<
+
+clean:
+	rm *.run.xml* *.blg *.out *.log *.aux *.bcf *.bbl *.nav *.toc *.vrb'
+)	
+	
+	
+if(is.function(output)) {
+		output(result)
+	}
+	if(is.character(output)) {
+		cat(result, file=output)
+	}
+	invisible(result)
 }
