7e51d78b154c2eb82b5680da6086c9c92bad0680
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-07 13:21:49 +0000

    spatio-temporal (with population a list) works when model is a vector
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1510 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/diseasemapping/R/getSMR.R
+++ pkg/diseasemapping/R/getSMR.R
@@ -412,52 +412,55 @@ function(popdata, model, casedata, regionCode,
     years = NULL, personYears=TRUE,year.range = NULL,...){
 #  lennon's stuff
   #isSP = (class(popdata[[1]]) == "SpatialPolygonsDataFrame")
+			
+			if (is.null(years)) {
+				years = as.integer(names(popdata))
+			}        
+			
+			if(!is.vector(model)) {
+				yearVar = grep("year", names(attributes((terms(model)))$dataClasses) ,
+						value=TRUE,ignore.case=TRUE)  # find year var in the model
+				if (length(model$sexSubset) == 1) message("only one sex is being used:",model$sexSubset)
+			} else {
+				yearVar = NULL
+			}
+			#If year is not in the model as factor
+			if(!length(yearVar)) yearVar="YEAR"
+			
+			caseYearVar = grep("year",names(casedata),value=TRUE,ignore.case=TRUE)
   
-  if (is.null(years)) {
-    years = as.integer(names(popdata))
-  }        
   
-  yearVar = grep("year", names(attributes((terms(model)))$dataClasses) ,
-      value=TRUE,ignore.case=TRUE)  # find year var in the model
-  #If year is not in the model as factor
-  if(length(yearVar)==0) yearVar="YEAR"
-  
-  caseYearVar = grep("year",names(casedata),value=TRUE,ignore.case=TRUE)
-  
-  
-  if(personYears){
-    # if year.range is missing, use year range of the cases
-    if(is.null(year.range)) {
-      if(!length(caseYearVar))
-        warning("year.range unspecified and no year column in case data")
-      year.range = range(as.numeric(as.character(casedata[,caseYearVar])), na.rm=TRUE)
-    }
-    
-    times <- c(year.range[1], sort(years), year.range[2])
-    times <- as.numeric(times)
-    inter <- diff(times)/2
-    nseq <- 1:length(inter) - 1
-    mseq <- 2:length(inter)
-    interval <- inter[mseq] + inter[nseq]
-    names(interval) <- names(popdata)
-  }else{
-    interval<-rep(1,length(popdata))
-  }
-  
-  names(interval) <- names(popdata)
+			if(personYears){
+				# if year.range is missing, use year range of the cases
+				if(is.null(year.range)) {
+					if(!length(caseYearVar))
+						warning("year.range unspecified and no year column in case data")
+					year.range = range(as.numeric(as.character(casedata[,caseYearVar])), na.rm=TRUE)
+				}
+				
+				times <- c(year.range[1], sort(years), year.range[2])
+				times <- as.numeric(times)
+				inter <- diff(times)/2
+				nseq <- 1:length(inter) - 1
+				mseq <- 2:length(inter)
+				interval <- inter[mseq] + inter[nseq]
+			}else{
+				interval<-rep(1,length(popdata))
+			}
+			names(interval) <- names(popdata)
   
   
   caseSexVar =grep("^sex$",names(casedata),value=TRUE,ignore.case=TRUE)
-  if (length(model$sexSubset) == 1) warning("only one sex is being used:",model$sexSubset)
-  
-  
   
   result=list()
   
-  area = !length(grep("longlat", popdata[[1]]@proj4string@projargs))
-  
-  #for(Dyear in seq(1, length(popdata))) {
-  for(Dyear in names(popdata)) {
+		if(length(grep("Spatial*DataFrame", class(popdata[[1]])))) {
+	  area = !length(grep("longlat", popdata[[1]]@proj4string@projargs))
+		} else {
+			area = FALSE
+		} 
+
+		for(Dyear in names(popdata)) {
     
     #Compute area
     if(area & class(popdata[[Dyear]]) == "SpatialPolygonsDataFrame") {
@@ -467,7 +470,7 @@ function(popdata, model, casedata, regionCode,
     
     
     # scaling factor to convert to person years
-    attributes(popdata[[Dyear]]@data)$popScale = interval[names(interval)==Dyear]
+    popScale = interval[names(interval)==Dyear]
     
     # add year column
     #popdata[[Dyear]][[yearVar]] = factor(rep(as.character(names(popdata[Dyear])), length(popdata[[Dyear]][1]),
@@ -478,9 +481,11 @@ function(popdata, model, casedata, regionCode,
     
     caseThisYear = casedata[casedata[[caseYearVar]]==Dyear,]
     
-    if (length(model$sexSubset) == 1) {      
-      caseThisYear = caseThisYear[caseThisYear[[caseSexVar]] == model$sexSubset, ]
-    }
+				if(!is.vector(model)) {
+					if (length(model$sexSubset) == 1) {      
+						caseThisYear = caseThisYear[caseThisYear[[caseSexVar]] == model$sexSubset, ]
+					}
+				}
     
     if(dim(caseThisYear)[1]==0) caseThisYear<-NULL   
     
