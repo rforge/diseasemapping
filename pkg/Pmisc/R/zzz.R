be01973eb43e36d4a2ebff684ec47e0a29ad5548
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-15 00:34:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1519 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Pmisc/R/zzz.R
+++ pkg/Pmisc/R/zzz.R
@@ -18,31 +18,51 @@
 			)
 	)
 	
-	if(FALSE) { # need version of R which handles #\VignetteBuilder metadata
-	# spin goat's hair
-	tools::vignetteEngine(
-			name = 'spin', 
-			package='Pmisc',
-			weave =	function(file, ...) {
-				knitr::spin(hair=file, format='Rmd', knit=TRUE, report=TRUE)
-			},
-			tangle = function(file, ...) file.copy(file, gsub("hair$", "", file)), 
-			pattern = '[.][Rr]hair$'
-	)
-	
-	# spin to an md file (don't convert to html)
-	tools::vignetteEngine(
-			name = 'spinReportFalse',
-  		package='Pmisc',
-			weave =	function(file, ...) {
-				# create an empty .tex file.  
-				# this is a hack because R CMD build doesn't
-				# check for .md files
-				file.create(gsub("\\.[[:alpha:]]+$", ".tex", file))
-				knitr::spin(hair=file, format='Rmd', knit=TRUE, report=FALSE)
-			},
-			tangle = function(file, ...) file.copy(file, gsub("hair$", "", file)),
-			pattern = '[.][Rr]hair$'
-	)
-}
+	if(TRUE) { # need version of R which handles #\VignetteBuilder metadata
+		# spin goat's hair
+		tools::vignetteEngine(
+				name = 'spin', 
+				package='Pmisc',
+				weave =	function(file, ...) {
+					knitr::spin(hair=file, format='Rmd', knit=TRUE, report=TRUE)
+				},
+				tangle = function(file, ...) {
+					hair=readLines(file)
+					wool=knitr::spin(
+							text=hair, 
+							format='Rmd', knit=FALSE)
+					knitr::knit(
+							text=wool,
+							tangle=TRUE, 
+							output = gsub("hair$", "", file)	
+					)
+				}, 
+				pattern = '[.][Rr]hair$'
+		)
+		
+		# spin to an md file (don't convert to html)
+		tools::vignetteEngine(
+				name = 'spinReportFalse',
+  			package='Pmisc',
+				weave =	function(file, ...) {
+					# create an empty .tex file.  
+					# this is a hack because R CMD build doesn't
+					# check for .md files
+					file.create(gsub("\\.[[:alpha:]]+$", ".tex", file))
+					knitr::spin(hair=file, format='Rmd', knit=TRUE, report=FALSE)
+				},
+				tangle = function(file, ...) {
+					hair=readLines(file)
+					wool=knitr::spin(
+							text=hair, 
+							format='Rmd', knit=FALSE)
+					knitr::knit(
+							text=wool,
+							tangle=TRUE, 
+							output = gsub("hair$", "", file)
+					)
+				},
+				pattern = '[.][Rr]hair$'
+		)
+	}
 }
