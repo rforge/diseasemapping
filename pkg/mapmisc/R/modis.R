e68175f079823d1bf42c37d7d7381214532a438b
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-01-23 04:04:54 +0000

    degree raster
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1615 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mapmisc/R/modis.R
+++ pkg/mapmisc/R/modis.R
@@ -1,33 +1,60 @@
 
 
 crsModis <- CRS(
-"+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
-	)
+  "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
+)
 
 getModisRaster = function() {
-modisRaster = raster(
-		extent(-20015109.354,20015109.354,-10007554.677,10007554.677),
-		nrow=18, ncol=36,
-		crs=crsModis		
-		)
-		
-values(modisRaster)= 1:ncell(modisRaster)
-		modisDf = data.frame(
-		ID = values(modisRaster),
-		h=colFromCell(modisRaster, 1:ncell(modisRaster))-1,
-		v=rowFromCell(modisRaster, 1:ncell(modisRaster))-1
-		)
-modisDf$hString = sprintf("%02d", modisDf$h)
-modisDf$vString = sprintf("%02d", modisDf$v)
+  modisRaster = raster(
+    extent(-20015109.354,20015109.354,-10007554.677,10007554.677),
+    nrow=18, ncol=36,
+    crs=crsModis		
+  )
+  
+  values(modisRaster)= 1:ncell(modisRaster)
+  modisDf = data.frame(
+    ID = values(modisRaster),
+    h=colFromCell(modisRaster, 1:ncell(modisRaster))-1,
+    v=rowFromCell(modisRaster, 1:ncell(modisRaster))-1
+  )
+  modisDf$hString = sprintf("%02d", modisDf$h)
+  modisDf$vString = sprintf("%02d", modisDf$v)
+  
+  modisDf$tile = paste(
+    'h', modisDf$hString, 'v', modisDf$vString, sep=''
+  )
+  
+  modisRaster = as.factor(modisRaster)
+  levels(modisRaster) = list(modisDf)
+  modisRaster
+}
 
-modisDf$tile = paste(
-		'h', modisDf$hString, 'v', modisDf$vString, sep=''
-		)
 
-modisRaster = as.factor(modisRaster)
-levels(modisRaster) = list(modisDf)
-modisRaster
+getDegreeRaster = function() {
+  degreeRaster = raster(
+    extent(c(-180,180,-90,90)),
+    res=1, crs=mapmisc::crsLL
+  )
+  values(degreeRaster)=1:ncell(degreeRaster)  
+# coordinate is bottom left
+  degreeMatXY = xyFromCell(degreeRaster, 1:ncell(degreeRaster)) - 0.5
+  degreeDf = data.frame(
+    ID=1:nrow(degreeMatXY),
+    x=degreeMatXY[,'x'],
+    y=degreeMatXY[,'y'],
+    ns = paste(c('n','s')[1+(degreeMatXY[,'y'] < 0 ) ], 
+      sprintf("%03d", abs(degreeMatXY[,'y']) ), sep=''
+    ),
+    ew = paste(c('e','w')[1+(degreeMatXY[,'x'] < 0 ) ], 
+      sprintf("%03d", abs(degreeMatXY[,'x']) ), sep=''
+    ),
+    stringsAsFactors=FALSE
+  )
+  degreeDf$tile = paste(degreeDf$ns, degreeDf$ew, sep='')
+  degreeRaster = ratify(degreeRaster)
+  levels(degreeRaster)[[1]] = degreeDf
+  degreeRaster
 }
 
 modisRaster = getModisRaster()
-
+degreeRaster = getDegreeRaster()
