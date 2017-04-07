3a4929048c251a307548c0371f61eb7d1b5f695c
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-04-07 00:53:24 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1700 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mapmisc/tests/perspective.R
+++ pkg/mapmisc/tests/perspective.R
@@ -10,37 +10,36 @@ hk = y = mapmisc::geocode("Hong Kong")
 guam = mapmisc::geocode("Guam")
 x = SpatialPoints(cbind(130,15), proj4string=crsLL)
 
-
 myProj = tpers(x=bind(x,y), hKm = 2*1000,
   tilt =-15, axis='seu')
 
 if(length(attributes(myProj)$ellipseSafeLL)>2) {
   
-myMap1 = openmap(
-  x=attributes(myProj)$regionLL[1,],
-  crs=crsMerc, zoom=3,
-path='https://sat01.maps.yandex.net/tiles?l=sat&v=1.35.0&')
+  myMap1 = openmap(
+    x=attributes(myProj)$regionLL[1,],
+    crs=crsMerc, zoom=3,
+    path='https://sat01.maps.yandex.net/tiles?l=sat&v=1.35.0&')
 #  path='https://services.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/')
-
-myMap2 = openmap(
-  x=attributes(myProj)$regionLL[2,],
-  zoom=attributes(myMap1)$tiles$zoom,
-  crs=crsMerc, zoom=3,
-path='https://sat01.maps.yandex.net/tiles?l=sat&v=1.35.0&')
+  
+  myMap2 = openmap(
+    x=attributes(myProj)$regionLL[2,],
+    zoom=attributes(myMap1)$tiles$zoom,
+    crs=crsMerc, zoom=3,
+    path='https://sat01.maps.yandex.net/tiles?l=sat&v=1.35.0&')
 #  path='https://services.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/')
-
-mapRaster = raster(
-  extent(attributes(myProj)$ellipse), 
-  res = mean(res(myMap1)/1.5),
-  crs=myProj
-)
-
-myMap1a = projectRaster(
-  from=myMap1, to=mapRaster, method='ngb')
-myMap2a = projectRaster(
-  from=myMap2, to=mapRaster, method='ngb')
-
-myMap = mosaic(myMap1a, myMap2a, fun=mean)
+  
+  mapRaster = raster(
+    extent(attributes(myProj)$ellipse), 
+    res = mean(res(myMap1)/1.5),
+    crs=myProj
+  )
+  
+  myMap1a = projectRaster(
+    from=myMap1, to=mapRaster, method='ngb')
+  myMap2a = projectRaster(
+    from=myMap2, to=mapRaster, method='ngb')
+  
+  myMap = mosaic(myMap1a, myMap2a, fun=mean)
 } else {
   myMap = openmap(
     x=attributes(myProj)$regionLL,
@@ -48,26 +47,15 @@ myMap = mosaic(myMap1a, myMap2a, fun=mean)
     crs=myProj,
     path='https://sat01.maps.yandex.net/tiles?l=sat&v=1.35.0&')
 #    path='https://services.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/')
-  }
-
-data("wrld_simpl", package='maptools')
-
-wrld4 = wrapPoly(x=wrld_simpl, crs=myProj)
+}
 
 
-
-xProj = spTransform(
-  guam, myProj
-)
+xProj = spTransform(guam, myProj)
 yProj = spTransform(hk, myProj)
-okinawaProj = spTransform(
-  okinawa, myProj
-)
+okinawaProj = spTransform(okinawa, myProj)
 
 
 if(!interactive()) pdf("perspective.pdf")
-
-
 
 map.new(myProj, bg='black', buffer=2*100*1000)
 plotRGB(myMap, add=TRUE, bgalpha=0)
@@ -87,12 +75,14 @@ text(yProj@coords,
   as.character(yProj$originalPlace),
   pos=2, col='yellow')
 
-plot(wrld4, add=TRUE)
+if(requireNamespace('maptools')) {
+  data("wrld_simpl", package='maptools')
+  wrld4 = wrapPoly(x=wrld_simpl, crs=myProj)
+  plot(wrld4, add=TRUE)
+}
 
 gridlinesWrap(myProj, col='yellow', 
   north=seq(-40,40,by=10), easts=seq(-150,180,by=10),
   lty=2)
-
-
 
 if(!interactive()) dev.off()
