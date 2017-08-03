0a206321e36992d1dfb50087981031b044894ffb
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-08-03 14:48:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1815 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Pmisc/inst/docs/inlaRemoteTest.R
+++ pkg/Pmisc/inst/docs/inlaRemoteTest.R
@@ -5,21 +5,31 @@ library("INLA")
 #'
 
 #+ iniFileLinux
-cat('
-RemoteINLA="/home/patrick/R/x86_64-pc-linux-gnu-library/3.4/INLA/bin/linux/64bit/inla"
-RemoteUserHost=patrick@darjeeling.pbrown.ca
-SSHCMD="ssh"
-SCPCMD="scp"
-', file="~/.inlarc")
+if(.Platform$OS.type == 'unix')
+  cat(
+      'RemoteINLA="/home/patrick/R/x86_64-pc-linux-gnu-library/3.4/INLA/bin/linux/64bit/inla"',
+      'RemoteUserHost="patrick@darjeeling.pbrown.ca"',
+      'SSHCMD="ssh"',    
+      'SCPCMD="scp"',
+      sep='\n', 
+      file="~/.inlarc")
 #'
 
 #+ iniFileWindows
-cat('
-    RemoteINLA="/home/patrick/R/x86_64-pc-linux-gnu-library/3.4/INLA/bin/linux/64bit/inla"
-    RemoteUserHost=patrick@darjeeling.pbrown.ca
-    SSHCMD="ssh"
-    SCPCMD="scp"
-    ', file="~/.inlarc")
+if(.Platform$OS.type == 'windows') {
+  INLA::inla.setOption("cygwin",
+      "C:\\\\Users\\\\brownpa\\\\programs\\\\cygwin"
+  )
+  cat(    
+      'RemoteINLA="/home/patrick/R/x86_64-pc-linux-gnu-library/3.4/INLA/bin/linux/64bit/inla"',
+      'RemoteUserHost="patrick@darjeeling.pbrown.ca"',
+      'SSHCMD="/cygdrive/c/Users/brownpa/programs/x2go/plink"',
+      'SCPCMD="/cygdrive/c/Users/brownpa/programs/x2go/pscp"',
+      sep='\n', 
+      file=normalizePath(file.path(
+              Sys.getenv("USERPROFILE"), 
+              ".inlarc"), '/', FALSE))
+}
 #'
 
 
@@ -29,18 +39,18 @@ inlaOrigBak = paste(inlaOrig, '.bak', sep='')
 for(D in 1:length(inlaOrig)) {
   file.copy(inlaOrig[D], inlaOrigBak[D], overwrite=FALSE)
   file.copy(
-    system.file('src/inla.remote.simple', package='Pmisc'),
-    inlaOrig[D],
-    overwrite=TRUE
+      system.file('src/inla.remote.simple', package='Pmisc'),
+      inlaOrig[D],
+      overwrite=TRUE
   )
 }
 #'
 
 #+ runInla
 res = inla(y ~ 1 + x, 
-  data = data.frame(y=1:3,x=factor(c("a","b",NA))), 
-  verbose=TRUE,
-  inla.call="remote")
+    data = data.frame(y=1:3,x=factor(c("a","b",NA))), 
+    verbose=TRUE,
+    inla.call="remote")
 #'
 
 #+ results
