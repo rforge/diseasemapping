74d0a641e6d8f13b66aba73ad1e88424e71b560c
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-02-09 17:08:51 +0000

    register
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1924 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/geostatsp/src/register.c
@@ -0,0 +1,50 @@
+
+#include"geostatsp.h"
+
+//maternPoints, distance gmrflik
+static const R_CallMethodDef callMethods[]  = {
+  {"maternPoints", (DL_FUNC) &maternPoints, 3},
+  {"maternDistance", (DL_FUNC) &maternDistance, 3},
+  {"gmrfLik", (DL_FUNC) &gmrfLik, },
+  {NULL, NULL, 0}
+};
+
+
+// matern
+static R_NativePrimitiveArgType matern_t[] = {
+    REALSXP, INTSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, INTSXP, REALSXP
+};
+
+// maternAniso
+static R_NativePrimitiveArgType maternAniso_t[] = {
+    REALSXP, REALSXP, INTSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP,  INTSXP, REALSXP
+};
+
+// maternArasterBpoints
+static R_NativePrimitiveArgType maternArasterBpoints_t[] = {
+    REALSXP, REALSXP, INTSXP, REALSXP, REALSXP, INTSXP,  REALSXP, REALSXP, INTSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP
+};
+
+// maternLogL
+static R_NativePrimitiveArgType maternLogL_t[] = {
+    REALSXP, REALSXP, REALSXP, INTSXP, REALSXP, INTSXP, REALSXP, INTSXP, REALSXP, REALSXP, REALSXP, REALSXP, INTSXP
+};
+
+// maternLogLopt
+static R_NativePrimitiveArgType maternLogLOpt_t[] = {
+    REALSXP, INTSXP, REALSXP, REALSXP, REALSXP, INTSXP, INTSXP, INTSXP, INTSXP, REALSXP, REALSXP, INTSXP, STRSXP
+};
+
+static const R_CMethodDef cMethods[] = {
+   {"myC", (DL_FUNC) &matern, 9, matern_t},
+   {"myC", (DL_FUNC) &maternAniso, 12, maternAniso_t},
+   {"myC", (DL_FUNC) &maternArasterBpoints, 15, maternArasterBpoints_t},
+   {"myC", (DL_FUNC) &maternLogL, 13, maternLogL_t},
+   {"myC", (DL_FUNC) &maternLogLOpt, 13, maternLogLOpt_t},
+   {NULL, NULL, 0, NULL}
+};
+
+void R_init_geostatsp(DllInfo* info) {
+	R_registerRoutines(info, NULL, NULL, NULL, NULL);
+	R_useDynamicSymbols(info, TRUE);
+}
