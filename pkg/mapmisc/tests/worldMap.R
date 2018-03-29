4c5d4bc07507acbb147dc4b4d60d21fcfb2fea4b
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-03-29 17:27:18 +0000

    fixing the caching, should solve cran errors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@1964 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mapmisc/tests/worldMap.R
+++ pkg/mapmisc/tests/worldMap.R
@@ -19,7 +19,7 @@ getOption("mapmiscCacheReadOnly")
 #'
 
 
-#+ themaps
+#+ themaps, fig.cap='some maps', fig.subcap = c('projectable region', 'projected, with bg','projected, with world countries','projectable madagascar','madagascar')
 Spackages = c('rgdal', 'rgeos', 'geosphere', 'maptools')
 
 if(all(unlist(mapply(requireNamespace, package=Spackages, MoreArgs=list(quietly=TRUE))))){
@@ -46,7 +46,7 @@ if(all(unlist(mapply(requireNamespace, package=Spackages, MoreArgs=list(quietly=
 	
 	map.new(xTcrop, buffer=1000*1000)
  plot(attributes(myCrsO)$ellipse, add=TRUE, col='lightBlue', border='blue')
-	plot(xTcrop,add=TRUE)
+	plot(xTcrop,add=TRUE, col='grey')
 	plot(xTcrop[DcountryT,], col='red', add=TRUE)
 	
 	gridlinesWrap(myCrsO, lty=2, col='orange')
@@ -60,14 +60,14 @@ if(all(unlist(mapply(requireNamespace, package=Spackages, MoreArgs=list(quietly=
 	
 	
 	plot(wrld_simpl)
-	plot(attributes(myCrsMoll)$crop, col='red', border='yellow', add=TRUE)
+	plot(attributes(myCrsMoll)$crop, border='red', col='#0000FF10', add=TRUE)
 	
 	xTcrop = wrapPoly(x=wrld_simpl, crs=myCrsMoll)
 	DcountryT  = grep(country, xTcrop$NAME)
 	
 	map.new(xTcrop)
 	plot(attributes(myCrsO)$ellipse, add=TRUE, col='lightBlue', border='blue')
-	plot(xTcrop,add=TRUE)
+	plot(xTcrop,add=TRUE, col='grey')
 	plot(xTcrop[DcountryT,], col='green', add=TRUE)
 	
 	gridlinesWrap(crs=xTcrop, lty=2, col='red')
