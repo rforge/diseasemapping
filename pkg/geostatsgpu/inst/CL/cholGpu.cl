eb8451714c7b92a61fafff93e4ce219ba3cf52a5
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-12-12 02:27:28 +0000

    more chol
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2031 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/geostatsgpu/inst/CL/cholGpu.cl
+++ pkg/geostatsgpu/inst/CL/cholGpu.cl
@@ -157,3 +157,235 @@ __kernel void sumLog(
 	}
 
 }
+
+
+// crossprod_ij contains sum_k L_kj L_ij D_k
+// crossprodLocal must contain over Nlocal * Nrows^2 
+// entries
+__kernel void cholCrossprod(
+	__global double *A, 
+	__global double *diag,
+	__global double *crossprod,
+	__local double *crossprodLocal,
+	const int Dcol,
+	const int DcolNpad,
+	const int Nrows, // N - Dcol
+	const int NrowsSq,
+	const int NlocalSum// typicaly 4, 
+	) {
+
+	const int Dlocal = get_local_id(0);
+	const int Nlocal = get_local_size(0);
+	const int Dglobal = get_global_id(0);
+	const int Nglobal = get_global_size(0);
+
+// location in crossprodLocal to store the results
+	const int crossprodIndex = Dlocal*NrowsSq;
+
+	int Drow1, Drow2, Dk, Drow1Nrows, DkNpad, DlocalNsq;
+	double ADhere, Dhere;
+
+	// Dk loops through columns of A
+	// do the first column, 
+	// to initialize crossprodLocal
+	Dk = Dglobal;
+	Dhere = diag[Dk];
+	DkNpad = Dk * Npad + Dcol;
+
+	// Drow1 loops through rows from Dcol
+	for(Drow1 = 0; Drow1 < Nrows; Drow1++) {
+		Drow1Nrows = crossprodIndex + Drow1 * Nrows;
+		ADhere = A[DkNpad + Drow1] * Dhere;
+
+		for(Drow2 = Drow1; Drow2 < Nrows; Drow2++){
+			crossprodLocal[Drow1Nrows + Drow2] =
+				ADhere * A[DkNpad + Drow2]; 
+		}
+	}
+
+	// now loop through the rest of the columns
+	for(Dk = Dglobal+ Nglobal; Dk < Dcol; Dk += Nglobal) {
+		Dhere = diag[Dk];
+		DkNpad = Dk * Npad + Dcol;
+		for(Drow1 = 0; Drow1 < Nrows; Drow1++) {
+			Drow1Nrows = crossprodIndex + Drow1 * Nrows;
+			ADhere = A[DkNpad + Drow1] * Dhere;
+			for(Drow2 = Drow1; Drow2 < Nrows; Drow2++){
+				crossprodLocal[Drow1Nrows + Drow2] +=
+					ADhere * A[DkNpad + Drow2]; 
+			}
+	}
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	// sum over the local work items
+	if(Dlocal < NlocalSum) {
+		DlocalNsq = Dlocal * NrowsSq;
+		for(Dk = Dlocal + NlocalSum; Dk < Nlocal; 
+			Dk += NlocalSum) {
+			DkNpad = Dk * NrowsSq;
+
+			for(Drow1 = 0; Drow1 < Nrows; Drow1++) {
+			Drow1Nrows = Drow1 * NrowsSq;	
+			for(Drow2 = Drow1; Drow2 < Nrows; Drow2++){
+				crossprodLocal[
+					DlocalNsq + Drow1Nrows+ Drow2
+				] += crossprodLocal[
+					DlocalNsq +  
+					DkNpad + 
+					Drow1Nrows +
+					Drow2
+				];
+			}
+			}
+		}
+	}
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	// sum over the local work items
+	if(Dlocal == 0 ) {
+		for(Dk = 1; Dk < NlocalSum; Dk ++) {
+			DkNpad = Dk * NrowsSq;
+
+			for(Drow1 = 0; Drow1 < Nrows; Drow1++) {
+			Drow1Nrows = Drow1 * NrowsSq;	
+			for(Drow2 = Drow1; Drow2 < Nrows; Drow2++){
+				crossprodLocal[
+					Drow1Nrows + Drow2
+				] += crossprodLocal[
+					DkNpad + 
+					Drow1Nrows +
+					Drow2
+				];
+			}
+			}
+		}
+		// copy to global memory
+		DkNpad = get_group_id(0) * NrowsSq;
+	
+		for(Drow1 = 0; Drow1 < Nrows; Drow1++) {
+			Drow1Nrows = Drow1 * NrowsSq;	
+			for(Drow2 = Drow1; Drow2 < Nrows; Drow2++){
+				crossprod[
+					DkNpad + Drow1Nrows + Drow2
+				] += crossprodLocal[
+					Drow1Nrows +
+					Drow2
+				];
+			}
+		}
+	} // end Dlocal == 0
+
+}
+
+__kernel void cholSumCrossprod(
+	__global double *crossprod,
+	const int Nrows,
+	const int NrowsSq,
+	const int NtoAdd // number of groups in cholCrossprod
+	) {
+
+	const int Dlocal = get_local_id(0);
+	const int Dgroup = get_group_id(0);
+	const int Ngroups = get_group_size(0);
+	const int Nlocal = get_local_size(0);
+
+	int Drow1, Drow2, Dindex, Dsum;
+	double theSum;
+
+	for(Drow1 = Dgroup; Drow < Nrows; Drow += Ngroups) {
+		for(Drow2 = Drow1 + Dlocal; Drow2 < Nrows; 
+			Drow2 += Nlocal) {
+			
+			Dindex = Drow1 * Nrows + Drow2;
+			theSum = 0.0;
+			
+			for(Dsum = 0; Dsum < NtoAdd; Dsum++) {
+				theSum += crossprod[
+					Dsum * NrowsSq + Dindex];
+			}
+			
+			crossprod[Dindex] = theSum;
+		}
+	}
+}
+
+__kernel void cholFromCrossprod(
+	__global double *A, 
+	__global double *diag,
+	__global double *crossprod,
+	__local double *diagLocal,
+
+	// column of A to update
+	const int Dcol,
+
+	// column up to which the cross product was computed
+	const int colAtCrossprod,
+
+	// Dcol - colAtCrossprod
+	const int colSinceCrossprod,
+
+	// index for A[Dcol, colSinceCrossprod]
+	// = Dcol + colSinceCrossprod * Npad;
+	const int AforCrossprodStartJ,
+
+	// index for A[Dcol+1, Dcol]
+	const int AforUpdateStart,
+
+	const int NrowsCrossprod, // N - colAtCrossprod
+	const int Nrows, // N - Dcol
+	const int Npad
+	) {
+
+	const int Nglobal = get_global_size(0);
+	const int Dglobal = get_global_id(0);
+	const int Nlocal = get_local_size(0);
+
+	double Dcrossprod;
+	int Drow, Dk, AforCrossprodStartI;
+
+	for(Dk = get_local_id(0); Dk < colSinceCrossprod;
+		Dk+=Nlocal) {
+
+		Dcrossprod = A[
+				AforCrossprodStartJ + Dk * Npad
+				] * diag[colAtCrossprod + Dk];
+		diagLocal[Dk] = Dcrossprod;
+	}
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	// compute the diagonal
+	if(Dlocal == 0) {
+		diagHere = 0.0;
+		for(Dk = 0; Dk < colSinceCrossprod; Dk++) {
+			diagHere += diagLocal[Dk];
+		}
+		diagLocal[colSinceCrossprod + 1] = diagHere;
+	}
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	diagHere = diagLocal[colSinceCrossprod + 1];
+
+	if(Dglobal == 0) {
+		diag[Dcol] = diagHere;
+	}
+
+
+	for(Drow = Dglobal+1; Drow < Nrows; 
+		Drow += Nglobal) {
+
+		// update the crossprod
+		AforCrossprodStartI = 
+			AforCrossprodStartJ + Drow;
+		Dcrossprod = crossprod[
+			colSinceCrossprod + Drow];
+		for(Dk = 0; Dk < colSinceCrossprod; Dk++) {
+			Dcrossprod += A[
+				AforCrossprodStartI + Dk * Npad
+				] * diagLocal[Dk];
+		}
+
+		Dk = AforUpdateStart + Drow;
+		A[Dk] = (A[Dk] - Dcrossprod) / diagHere;
+	}
+
+}
--- pkg/geostatsgpu/src/cholGpu.cpp
+++ pkg/geostatsgpu/src/cholGpu.cpp
@@ -10,7 +10,7 @@ double cholGpu(
 	viennacl::matrix<double> &A,
 	// on exit the diagonal
 	viennacl::vector_base<double> &D,
-	// temporary storage, of length local_size
+	// temporary storage, of length number of groups
 	viennacl::vector_base<double> &diagWorking,
 	// store diag times rows of A, length N
 	viennacl::vector_base<double> &diagTimesRowOfA,
@@ -20,17 +20,27 @@ double cholGpu(
 	// kernels
 	viennacl::ocl::kernel &cholDiag,
 	viennacl::ocl::kernel &cholOffDiag,
+	viennacl::ocl::kernel &cholCrossprod,
+	viennacl::ocl::kernel &cholSumCrossprod,
+	viennacl::ocl::kernel &cholFromCrossprod,
 	viennacl::ocl::kernel &sumLog
 	){
 
-	double tempDouble; // the result
-	double NlocalStorateD = NlocalStorage;
+	double tempDouble; 
 	int Dcol, Dcolm1, Ncycles, Ncyclesm1, NbeforeLastCycle;
-	int err;
+	int err, Drow, DrowFromZero, Nrows;
 
 	const int 
 		Npad=A.internal_size2(),
 		N=A.size2();
+	const int Nm1 = N - 1, Nm2 = N-2;
+
+	// how many cross products can be stored in local memory
+	const double NlocalStorageD = sqrt(NlocalStorage);
+	const int Ncrossprod = floor(NlocalStorageD/
+		cholCrossprod.local_work_size());
+	const int NbeginLast = N - Ncrossprod;
+
 
 #ifdef UNDEF
 	Rcout << "diagLocalSize " << NlocalStorage <<
@@ -38,18 +48,34 @@ double cholGpu(
 #endif
 
 	// first column
-	viennacl::vector_base<double> firstColX(
-			A.handle(), N, 0, 1);
-	tempDouble = firstColX(0);
+	viennacl::vector_base<double> oneColX(
+			A.handle(), Nm1, 1, 1);
+	tempDouble = A(0,0);
 	D(0) = tempDouble;
-	firstColX *= (1 / tempDouble);
+	oneColX *= (1 / tempDouble);
 
+	// second column
+	tempDouble = A(1,0);
+	tempDouble = A(1,1) - tempDouble*tempDouble * D(0);
+	D(1) = tempDouble;
+	tempDouble = 1/tempDouble;
 
-// remaining columns
-	for(Dcol=1;Dcol<N;Dcol++) {
+	oneColX = vector_base(
+			A.handle(), Nm2, 2, 1);
+
+	viennacl::vector_base<double> twoColX(
+			A.handle(), Nm2, N+2, 1);
+
+	twoColX = ( -D(0) * A(1,0) * tempDouble ) * oneColX + 
+		tempDouble * twoColX; 
+
+	// remaining columns
+	// until the number of columns remaining
+	// is small enough to store in local memory
+	for(Dcol=2;Dcol<NbeginLast;Dcol++) {
 		
 		Dcolm1 = Dcol - 1;
-		Ncycles = ceil(Dcol / NlocalStorateD);
+		Ncycles = ceil(Dcol / NlocalStorageD);
 
 
 		Ncyclesm1 = Ncycles-1;
@@ -92,14 +118,38 @@ double cholGpu(
 			));
 	}
 
-#ifdef UNDEF
-	Rcout << "NlocalSize " << D(10) << 
-		" NlocalStorage " << D(11) << 
-		" " << D(12) << " " << D(13)<< 
-		" Nin " << D(14) << " dd " << D(15) <<
-		" Dcy " << D(16) << 
-		"\n";
-#endif
+	const int NrowsInCrossprod = N - NbeginLast;
+	const int NrowsInCrossprodSq = 
+		NrowsInCrossprod * NrowsInCrossprod;
+	const int NlocalSum = 4;
+
+	// compute the cross products
+	viennacl::ocl::enqueue(cholCrossprod(
+		A, D, diagTimesRowOfA, diagLocal,
+		NbeginLast, NbeginLast * Npad,
+		NrowsInCrossprod, NrowsInCrossprodSq,
+		NlocalSum));
+
+	viennacl::ocl::enqueue(cholSumCrossprod(
+		diagTimesRowOfA,
+		NrowsInCrossprod, NrowsInCrossprodSq,
+		cholCrossprod.global_work_size() / 
+			cholCrossprod.local_work_size()
+	));
+
+	const int Nrows = N - NbeginLast;
+
+	for(Dcol = NbeginLast; Dcol < Nm1; Dcol++) {
+
+		viennacl::ocl::enqueue(cholFromCrossprod(
+			A, D, diagTimesRowOfA, diagLocal,
+			Dcol, NbeginLast,
+			Dcol - NbeginLast,
+			Dcol + NbeginLast * Npad,
+			Dcol + 1 + Dcol * Npad,
+			Nrows, Npad
+			));
+	}
 
 	viennacl::linalg::opencl::matrix_diagonal_assign(A, 1.0);
 
@@ -116,6 +166,9 @@ double cholGpu(
 double cholGpu(
 	viennacl::matrix<double> &x,
 	viennacl::vector_base<double> &D,
+	// to do: add the two working arrays
+	// viennacl::vector_base<double> &diagWorking,
+	// viennacl::vector_base<double> &diagTimesRowOfA,
 	std::string kernel,
 	const int ctx_id,
 	const int MCglobal,
@@ -140,6 +193,9 @@ double cholGpu(
 	viennacl::ocl::kernel 
 		&cholDiag = my_prog.get_kernel("cholDiag"),
 		&cholOffDiag = my_prog.get_kernel("cholOffDiag"),
+		&cholCrossprod = my_prog.get_kernel("cholCrossprod"),
+		&cholSumCrossprod = my_prog.get_kernel("cholSumCrossprod"),
+		&cholFromCrossprod = my_prog.get_kernel("cholFromCrossprod"),
 		&sumLog = my_prog.get_kernel("sumLog");
 
 	// set global work sizes
@@ -153,16 +209,28 @@ double cholGpu(
 	sumLog.global_work_size(0, MCglobal);
 	sumLog.local_work_size(0, MClocal);
 
-	viennacl::ocl::local_mem Dlocal(
+	cholCrossprod.global_work_size(0, MCglobal);
+	cholCrossprod.local_work_size(0, MClocal);
+
+	cholSumCrossprod.global_work_size(0, MCglobal);
+	cholSumCrossprod.local_work_size(0, MClocal);
+
+	cholFromCrossprod.global_work_size(0, MCglobal);
+	cholFromCrossprod.local_work_size(0, MClocal);
+
+
+	viennacl::ocl::local_mem diagLocal(
 		localVectorSize*sizeof(cl_double));
+
 	viennacl::vector_base<double> Dworking(
 		ceil(MCglobal/MClocal)*sizeof(cl_double));
+
 	viennacl::vector_base<double> diagTimesRowOfA(
 		x.size1()*sizeof(cl_double));
 
 # ifdef UNDEF
 	Rcout <<
-		"e Dlocal size " << Dlocal.size() << 
+		"e Dlocal size " << diagLocal.size() << 
 		" global size " << cholOffDiag.global_work_size(0) << 
 		" local size " << cholOffDiag.local_work_size(0) << 
 		" local vector size" << localVectorSize << "\n";
@@ -172,10 +240,13 @@ double cholGpu(
 		x, D, 
 		Dworking, 
 		diagTimesRowOfA,
-		Dlocal,
+		diagLocal,
 		localVectorSize,
 		cholDiag,
 		cholOffDiag,
+		cholCrossprod,
+		cholSumCrossprod,
+		cholFromCrossprod,
 		sumLog);
 
 	return(logdet);
@@ -186,6 +257,9 @@ double cholGpu(
 SEXP cpp_cholGpu(
 	SEXP xR,
 	SEXP DR,
+	// to do: add the two working arrays
+	// SEXP diagWorkingR, ceil(MCglobal/MClocal)
+	// SEXP diagTimesRowOfAR, N
 	IntegerVector MCglobal,
 	IntegerVector MClocal,
   	IntegerVector localStorage,
