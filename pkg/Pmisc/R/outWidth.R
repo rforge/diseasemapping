43184cfcfa46918d704a7f67098da60f7e413141
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-02-12 15:31:15 +0000

    got rid of mdToTex
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2049 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Pmisc/R/outWidth.R
+++ pkg/Pmisc/R/outWidth.R
@@ -1,30 +1,21 @@
 #' Output width for knitr figures
 #' 
 #' @description Creates output width strings for html and latex
-#' @param x width, fraction of page width
-#' @param mdToTex if \code{FALSE} convert to markdown table, return \code{x} otherwise
+#' @param x width, fraction of page width, a number between zero and 1
+#' @param ... other arguments, currently ignored
 #' @examples 
-#' Pmisc::out.width(0.5, 'latex')
-#' Pmisc::out.width(0.5, 'markdown')
-#' mdToTex=FALSE
-#' Pmisc::out.width(0.5, 'auto')
-#' mdToTex=TRUE
-#' Pmisc::out.width(0.5, 'auto')
+#' Pmisc::out.width(0.5)
 #' 
 #' @export
-out.width = function(x, mdToTex = 'auto') {
-	if(identical(mdToTex, 'auto')) {
-		mdToTex = any(commandArgs()=='mdToTex', na.rm=TRUE)
-		if(exists('mdToTex')) {
-			mdToTex = identical(get("mdToTex"), TRUE)
-		}
-	}
-	
-	if(identical(mdToTex, TRUE) | identical(mdToTex, 'latex') ) {
-		
-		result = paste(x, '\\textwidth', sep='')
+out.width = function(x, ...) {
+
+	testPlotOutput = knitr::knit_hooks$get()$plot(
+		'xx', list(fig.align='default',fig.show=TRUE))
+
+	if(grepl("includegraphics[{]xx[}]", testPlotOutput)[1]) {
+		result = paste0(x, '\\textwidth')
 	} else {
-		result = paste(100*x, '%', sep='')
+		result = paste0(100*x, '%')
 	}
 	
 	result
