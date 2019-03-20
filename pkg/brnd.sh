7c0558261412f1659416916afed4da3357713b3f
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-03-20 20:15:37 +0000

    stuff
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2091 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/brnd.sh
@@ -0,0 +1,4 @@
+R -e "Rcpp::compileAttributes('clRNG')"
+R -e "devtools::document('clRNG')"
+R CMD build clRNG
+tar --extract --strip=3 --directory=junk --file=clRNG_0.0.1.tar.gz clRNG/inst/doc/runif.html
