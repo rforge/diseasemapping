68010d6f23b412ef40f9fecf8cbefb67e99d722d
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-05-25 11:44:55 +0000

    fisher sim added 1.0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2164 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/clRNG/src/random_number.cpp
+++ pkg/clRNG/src/random_number.cpp
@@ -1,72 +1,14 @@
 #include "clRNG.hpp"
 #include <clRNG/mrg31k3p.h>
 #include <CL/mrg31k3pkernelStringSeparate.hpp>
+#include "random_number.hpp"
+
 
 using namespace Rcpp;
 using namespace viennacl;	
 using namespace viennacl::linalg;
 
-// clRNG -> Matrix
-void convertclRngMat(clrngMrg31k3pStream* streams, Rcpp::IntegerMatrix result) {
-  
-  int numWorkItems = result.nrow();
-  int Ditem,Delement,Dcis,Dg;
-  for(Ditem =0;Ditem < numWorkItems;Ditem++){
-    for(Delement=0;Delement < 3;Delement++){
-      
-      Dcis=0;
-      Dg=0;
-      result(Ditem, Dcis*6 + Dg*3 + Delement) = streams[Ditem].current.g1[Delement];//0,0; 0,1
-      Dg=1;
-      result(Ditem,Dcis*6 + Dg*3 + Delement) = streams[Ditem].current.g2[Delement];//0,3; 0,4
-      
-      Dcis=1;
-      Dg=0;
-      result(Ditem,Dcis*6 + Dg*3 + Delement) = streams[Ditem].initial.g1[Delement];//0,6; 0,7
-      Dg=1;
-      result(Ditem,Dcis*6 + Dg*3 + Delement) = streams[Ditem].initial.g2[Delement];//0,9; 0,10
-      
-      Dcis=2;
-      Dg=0;
-      result(Ditem,Dcis*6 + Dg*3 + Delement) = streams[Ditem].substream.g1[Delement];//0,12
-      Dg=1;
-      result(Ditem,Dcis*6 + Dg*3 + Delement) = streams[Ditem].substream.g2[Delement];//0,15
-    }
-  }
-  
-}
 
-//matrix ->clRNG streams
-void convertMatclRng(Rcpp::IntegerMatrix Sin, clrngMrg31k3pStream* streams){
-  
-  int Ditem,Delement,Dcis,Dg;
-  int numWorkItems = Sin.nrow();
-  
-  for(Ditem =0;Ditem < numWorkItems;Ditem++){
-    for(Delement=0;Delement < 3;Delement++){
-      
-      Dcis=0;
-      Dg=0;
-      streams[Ditem].current.g1[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      Dg=1;
-      streams[Ditem].current.g2[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      
-      Dcis=1;
-      Dg=0;
-      streams[Ditem].initial.g1[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      Dg=1;
-      streams[Ditem].initial.g2[Delement]=Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      
-      Dcis=2;
-      Dg=0;
-      streams[Ditem].substream.g1[Delement]=Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-      Dg=1;
-      streams[Ditem].substream.g2[Delement] = Sin(Ditem,Dcis*6 + Dg*3 + Delement);
-    }
-  }
-  
-  
-}
 
 
 template <typename T> 
@@ -208,8 +150,11 @@ void runifGpuHost(viennacl::vector_base<double> &x)//use them to generate number
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+
 //[[Rcpp::export]]
-Rcpp::IntegerMatrix  cpp_mrg31k3pCreateStreams(int numWorkItems) 
+Rcpp::IntegerMatrix  cpp_mrg31k3pCreateStreams(int numWorkItems) //this function returns a R_stream not clrng stream
 {
   
   Rcpp::IntegerMatrix result=Rcpp::IntegerMatrix(numWorkItems,18L);
@@ -254,6 +199,7 @@ SEXP random_numberGpu(
   
   return(Rcpp::wrap(1L));	
 }
+
 
 //[[Rcpp::export]]
 SEXP cpp_random_numberGpu(
