cf872525485ee8e4dcebfa7247e3a9388d1f9a66
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-06-11 00:56:49 +0000

    stuff
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2167 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuMatrix/src/chol.cpp
+++ pkg/gpuMatrix/src/chol.cpp
@@ -1,8 +1,10 @@
 
+#include "gpuMatrix.hpp"
+
 
 
 template <typename T> 
-std::string multiplyLowerTypeString() {
+std::string cholMulti() {
 
   std::string typeString = openclTypeString<T>();
   std::string result = "";
@@ -12,77 +14,120 @@ std::string multiplyLowerTypeString() {
   }
   result = result + 
 "__kernel void cholGpu("
-"	__global double *A," 
-"	__global double *diag,"
-"	__local double *diagLocal,"
+"	__global " + typeString + " *A," 
+"	__global " + typeString + " *diag,"
+"	__local " + typeString + " *diagLocal,"
 "	const int N, "
-"	const int Npad,"
-"	const int NlocalStorage){"
-// first dimension is rows, second dimension is work items
+"	const int Npad, const int NpadDiag,\n",
+" const int NpadAA," //
+"	const int Nstop, const int Nmatrix){\n"
+// first dimension is rows, 
+
+// second dimension is work items
 // doing the same row
-"	const int Dglobal0 = get_global_id(0);"
-"	const int Dglobal1 = get_global_id(1);"
-"	const int Nsize0 = get_global_size(0);"
-"	const int Nsize1 = get_global_size(1);"
-"	const int NlocalSize = get_local_size(1);"
- 
+
+// third dimension matrix
+
+"	const int Dglobal0 = get_global_id(0);" // row
+" const int Nsize0 = get_global_size(0);"
+"	const int Dglobal1 = get_global_id(1);" // group within row
+" const int Nsize1 = get_global_size(1);"
+
+" const int doDiag = (Dglobal0 == 0);" // diagonals only done with first row
+" const int doDiagSum = (Dglobal0 == 0 & Dglobal1 == 0);\n"
+
+// groups work on the same row of the same matrix
+// only dimension 1 is different 
+" const int Dlocal = get_local_id(1);\n"
+" const int NlocalSize = get_local_size(1);\n"
+" const int doDiagLocal = (get_local_id(0) == 0);\n"
+// get_local_size(2) must be 1
+
 "	int Dcol=0, Dcolm1;"
 "	int DcolNpad = Dcol*Npad;"
-"	int maxDcolNlocalStorage = min(Dcol, NlocalStorage);"
 
-"	__private int Drow, Dk, DrowDk;"
-"	__private double DL, Ddiag;\n"
+"	__private int Drow, Dk, DrowDk, Dmatrix;"
+"	__private " + typeString + " DL, Ddiag, *AHere, *diagHere;\n"
 
-"for(Dcol = 0; Dcol < N; Dcol++){\n"
+"for(Dmatrix =  get_global_id(2); Dmatrix < Nmatrix; Dmatrix += get_global_size(2)) {"
+
+"AHere = &A[Dmatrix*NpadAA];\n"
+"diagHere = &diag[Dmatrix*NpadDiag];\n"
+
+"for(Dcol = 0; Dcol < Nstop; Dcol++){\n"
 "   Dcolm1 = Dcol-1;\n"
 "   DcolNpad = Dcol*Npad;\n"
 // diagonal entry, use only first group
-" if(Dglobal0 = 0){\n"
+" if(doDiag){\n"
 "	Ddiag = 0.0;\n"
 "	for(Dk = Dglobal1; Dk < Dcol; Dk += Nsize1) {\n"
-"		DL = A[Dcol + Dk * Npad];"
+"		DL = AHere[Dcol + Dk * Npad];"
 "		DL *= DL;"
-"		diagLocal[Dlocal] += diag[Dk] * DL;"
-"	}\n"
-"	}\n" // Dglobal0 == 0
+"		diagLocal[Dlocal] += diagHere[Dk] * DL;"
+"	}\n" // Dk
+"	}\n" // doDiag
 "	barrier(CLK_LOCAL_MEM_FENCE);\n"
 // add 'em up to get diagonal
-"	if(Dglobal0==0 & Dglobal1==0) {\n"
+"	if(doDiagSum) {\n"
 "		Ddiag = 0.0;\n"
 "		for(Dk = 0; Dk < NlocalSize; Dk++) {\n"
 "			Ddiag += diagLocal[Dk];\n"
 "		}\n"
-"		diag[Dcol] = A[Dcol + DcolNpad] - Ddiag;\n"
-"	}\n" // Dglobal1 == 0
-"	barrier(CLK_LOCAL_MEM_FENCE);\n"
+  // Ddiag is now diag[Dcol]
+  // copy it to global memory
+"		diagHere[Dcol] = AHere[Dcol + DcolNpad] - Ddiag;\n"
+"   AHere[Dcol + DcolNpad] = 1.0;"
+"	}\n" // doDiagSum
+"	barrier(CLK_GLOBAL_MEM_FENCE);\n" // all items need the diagonal
+" Ddiag = diagHere[Dcol];" 
  
 	// diagLocal =  diag * A[Dcol, ]
-	// TO DO: use less memory for diagLocal
-"	for(Dk = Dlocal; Dk < maxDcolNlocalStorage; Dk += NlocalSize) {\n"
-"		DL = A[Dcol + Dk * Npad];\n"
-"		diagLocal[Dk] = diag[Dk] * DL;\n"
+" if(doDiagLocal){\n"
+"	for(Dk = Dlocal; Dk < Dcol; Dk += NlocalSize) {\n"
+"		DL = Ahere[Dcol + Dk * Npad];\n"
+"		diagLocal[Dk] = diagHere[Dk] * DL;\n"
 "	}\n"
+" }\n" // doDiag
 "	barrier(CLK_LOCAL_MEM_FENCE);\n" 
 
-"	for(Drow = (N-Dglobal-1); Drow > Dcol; Drow -= Nsize) {\n"
 
-"		DL = A[Drow + DcolNpad];\n"
+"	for(Drow = (N-Dglobal-1); Drow > Dcol; Drow -= Nsize0) {\n"
 
-"		DrowDk=Dcolm1*Npad;\n"
+"		DL = AHere[Drow + DcolNpad];\n"
+
+"		DrowDk = Dcolm1*Npad;\n"
 "		for(Dk = Dcolm1; Dk >= 0; Dk--) {\n"
 //			DL -= A[DrowDk] * diagLocal[Dk];"
 "			DrowDk -= Npad;"
-"			DL -= A[Drow + Dk * Npad] * diagLocal[Dk];"
+"			DL -= AHere[Drow + Dk * Npad] * diagLocal[Dk];"
 			// DL -= A[Drow + Dk * Npad] * A[Dcol + DkNpad] * diag[Dk];"
 "		}\n" // Dk
-"		A[Drow + DcolNpad] = DL / Ddiag;"
+"		AHere[Drow + DcolNpad] = DL / Ddiag;"
 
 "	}\n" // Drow
-	// Ddiag is now diag[Dcol]
-	// copy it to global memory
-"	if(Dglobal == 0) {"
-"		diag[Dcol] = Ddiag;"
-"		A[Dcol + DcolNpad] = 1.0;"
-"	}\n"
+
+" barrier(CLK_GLOBAL_MEM_FENCE);\n" // all rows must be done before advancing to next column
+
 "}\n" // Dcol loop
-"}";
+" barrier(CLK_GLOBAL_MEM_FENCE);\n" 
+"}\n" // Dmatrix loop
+"}\n";
+
+
+template <typename T> 
+int cholMulti(
+  viennacl::matrix<T> *A,
+  viennacl::vector<T> *D,
+  const int Nmatrix,
+  const IntegerVector Nglobal,
+  const IntegerVector Nlocal, 
+  const int NlocalCache,
+  const int ctx_id) {
+
+  const bool BisVCL=1;
+
+  double **AA, **DD;
+  
+
+  return 0L;
+}
