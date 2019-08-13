9b12aa15f4727d39adc6bee02b82fe96f784af54
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-08-13 15:24:01 +0000

    box
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2230 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mapmisc/R/map.new.R
+++ pkg/mapmisc/R/map.new.R
@@ -23,11 +23,18 @@ map.new = function(x, legendRight=FALSE, buffer=0, mar=c(0,0,0,0), ...) {
 
 	ldots = list(...)
 	ldots$mar = mar
-	if(!length(ldots$bty) & mar[4]>0 ) ldots$bty='l'
+	if(!length(ldots$bty)) {
+		if( mar[4]>0 ) {
+			ldots$bty='l'
+		} else {
+			ldots$bty='n'			
+		}
 	if(!length(ldots$xpd)) ldots$xpd=FALSE
 	if(!length(ldots$axes)) ldots$axes=TRUE
 	if(!length(ldots$xaxs)) ldots$xaxs = 'i'
 	if(!length(ldots$yaxs)) ldots$yaxs = 'i'
+	if(!length(ldots$xaxt)) ldots$yaxs = 'n'
+	if(!length(ldots$yaxt)) ldots$yaxs = 'n'
 	
   	do.call(par, ldots[setdiff(names(ldots),'axes')]) #(mar=mar, xaxs='i',yaxs='i',xpd=FALSE, ...)
   
