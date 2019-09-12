7af97b8fbb3b769223d9efcbc12c6d8bc2ca2b62
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-09-12 04:25:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2255 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/R/gpuFiser_test.R
+++ pkg/gpuRandom/R/gpuFiser_test.R
@@ -1,49 +1,49 @@
-#' @title gpuFisher_test
+#' @title Fisher's exact test on GPU
 #'
-#' @description Peform Fisher's simulation test on GPU
+#' @description Peform Fisher's exact test with Monte Carlo simulation on GPU.
 #'
-#' @param sr a Vclvector
-#' @param sc a Vclvector
-#' @param x a Vclvector
-#' @param streams vector of integers, 18x workgroupSize
-#' @param workgroupSize global dimensions of work items
-#' @param localSize dimensions of work groups
-#' @return altered in-place
+#' @param sr A Vclvector specifying the marginal row totals
+#' @param sc A Vclvector specifying the marginal column totals
+#' @param x A Vclvector for putting test statistics.
+#' @param streams Vectors of integers, 18x workgroupSize
+#' @param workgroupSize Global dimensions of work-item domain
+#' @param localSize Dimensions of work groups
+#' @return Altered in-place
 #' @useDynLib gpuRandom
 #' @export
 
-fisher_simGpu=function(
+gpuFisher_test=function(
   sr, #marginal row total 
   sc, #marginal column total
-  x, # the vector to store test statistics,//extraX,
+  x, #extraX,
   streams, 
   workgroupSize,
   localSize){
   
   verbose = TRUE
   
-  
   if(missing(streams)) {
     if(missing(workgroupSize)) 
-    {workgroupSize = c(64,4)
-    streams = cpp_mrg31k3pCreateStreams(prod(workgroupSize))	}	
-  } else {
-    if(!is.matrix(streams)) {
-      warning("streams should be a matrix")
-    }
+    {workgroupSize = c(64,4)}
+    streams = cpp_mrg31k3pCreateStreams(prod(workgroupSize))	
+  }else{
+    if(!is.matrix(streams))
+      {warning("streams should be a matrix")}
     
     if(prod(workgroupSize) != nrow(streams))
       warning("number of work items needs to be same as number of streams")
-    # make a deep copy
+
     streams = matrix(as.vector(streams), nrow(streams), ncol(streams), FALSE, dimnames(streams))
      }
-  localSize = pmax(2,c(localSize, 2, 2)[1:2])
+  
+  if (missing(localSize)){
+  localSize = pmax(2,c(localSize, 2, 2)[1:2])}
   
   if(verbose) {
     cat('local sizes ', toString(localSize), '\nglobal sizes ', toString(workgroupSize),
         '\n streams ', toString(dim(streams)), '\n')
   }
-  cpp_gpuFisher_test(sr, sc, x, #extraX, 
+  cpp_gpuFisher_test(sr, sc, x, #extraX
                      streams, workgroupSize,localSize)
   
   invisible(streams)
