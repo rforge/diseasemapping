a0099af7ec8fc197ff62321e410e8a542f1d5b8c
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-09-12 03:48:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2254 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/gpuRandom2/R/gpuFiser_test.R
@@ -0,0 +1,70 @@
+#' @title Fisher's exact test on GPU
+#'
+#' @description Peform Fisher's exact test with Monte Carlo simulation on GPU.
+#'
+#' @param sr A Vclvector specifying the marginal row totals
+#' @param sc A Vclvector specifying the marginal column totals
+#' @param x A Vclvector for putting test statistics.
+#' @param streams Vectors of integers, 18x workgroupSize
+#' @param workgroupSize Global dimensions of work-item domain
+#' @param localSize Dimensions of work groups
+#' @return Altered in-place
+#' @useDynLib gpuRandom
+#' @export
+
+gpuFisher_test=function(
+  sr, #marginal row total 
+  sc, #marginal column total
+  x, #extraX,
+  streams, 
+  workgroupSize,
+  localSize){
+  
+  verbose = TRUE
+  
+  if(missing(streams)) {
+    if(missing(workgroupSize)) 
+    {workgroupSize = c(64,4)}
+    streams = cpp_mrg31k3pCreateStreams(prod(workgroupSize))	
+  }else{
+    if(!is.matrix(streams))
+      {warning("streams should be a matrix")}
+    
+    if(prod(workgroupSize) != nrow(streams))
+      warning("number of work items needs to be same as number of streams")
+
+    streams = matrix(as.vector(streams), nrow(streams), ncol(streams), FALSE, dimnames(streams))
+     }
+  
+  if (missing(localSize)){
+  localSize = pmax(2,c(localSize, 2, 2)[1:2])}
+  
+  if(verbose) {
+    cat('local sizes ', toString(localSize), '\nglobal sizes ', toString(workgroupSize),
+        '\n streams ', toString(dim(streams)), '\n')
+  }
+  cpp_gpuFisher_test(sr, sc, x, #extraX
+                     streams, workgroupSize,localSize)
+  
+  invisible(streams)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
