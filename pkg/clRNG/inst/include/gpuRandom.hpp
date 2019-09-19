a2d70b0103ebd1980dd9ef5870f8134c14003cd8
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-09-19 17:51:47 +0000

    stuff
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2263 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/clRNG/inst/include/gpuRandom.hpp
+++ pkg/clRNG/inst/include/gpuRandom.hpp
@@ -13,3 +13,11 @@
 //from typeDef.cpp
 template <typename T> std::string openclTypeString();
 template <typename T> int sizeOfReal();
+
+
+// clRNG -> Matrix
+void convertclRngMat(clrngMrg31k3pStream* streams, Rcpp::IntegerMatrix result);
+//matrix ->clRNG streams
+void convertMatclRng(Rcpp::IntegerMatrix Sin, clrngMrg31k3pStream* streams);
+
+template <typename T> std::string mrg31k3pTypeString();
--- pkg/clRNG/src/random_number.hpp
+++ pkg/clRNG/src/random_number.hpp
@@ -2,9 +2,3 @@
 #include <gpuRandom.hpp>
 
 
-// clRNG -> Matrix
-void convertclRngMat(clrngMrg31k3pStream* streams, Rcpp::IntegerMatrix result);
-//matrix ->clRNG streams
-void convertMatclRng(Rcpp::IntegerMatrix Sin, clrngMrg31k3pStream* streams);
-
-template <typename T> std::string mrg31k3pTypeString();
