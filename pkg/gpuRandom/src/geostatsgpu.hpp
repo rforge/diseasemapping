db5212147b908f36481569d6a6222537195b2aae
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-09-25 02:01:53 +0000

    09/24
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2268 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/gpuRandom/src/geostatsgpu.hpp
@@ -0,0 +1,21 @@
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+
+//#include <string>
+#include <Rcpp.h>
+
+#include <gpuMatrix/dynVCLMatGeostatsgpu.hpp>
+#include <gpuMatrix/dynVCLVecGeostatsgpu.hpp>
+
+#include "viennacl/ocl/backend.hpp"
+#include "viennacl/linalg/sum.hpp"
+//#include "viennacl/linalg/lu.hpp"
+
+
+extern "C" void Rtemme_gamma(double *nu, double * g_1pnu, double * g_1mnu, double *g1, double *g2);
+
+template<typename T> double luT(viennacl::matrix<T> &vclX, viennacl::vector_base<T> &vclD);
+
+
+template <typename T> int sizeOfReal();
+template <typename T> std::string openclTypeString();
+template <typename T> T maternClEpsilon();
