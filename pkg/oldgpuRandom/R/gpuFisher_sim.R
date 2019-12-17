305f18a080a16f9da5c696b15a8b3c5c42d4b71a
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-12-17 03:48:06 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2343 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/oldgpuRandom/R/gpuFisher_sim.R
@@ -0,0 +1,137 @@
+#' @title Fisher's exact test on GPU
+#'
+#' @useDynLib gpuRandom
+#' @export
+
+
+
+fisher.sim=function(
+  x,
+  B,# number of simualtion,
+  type = c('float','double')[1+gpuR::gpuInfo()$double_support],
+  returnStatistics = FALSE,
+  streams, 
+  workgroupSize,
+  localSize=c(2,2),
+  verbose = FALSE){
+
+  METHOD <- "Fisher's Exact Test for Count Data"
+  
+  if (is.data.frame(x))
+    x <- as.matrix(x)
+  
+  if(is.matrix(x)) {
+    if(any(dim(x) < 2L))
+      stop("'x' must have at least 2 rows and columns")
+    
+    if(!is.numeric(x) || any(x < 0) || any(is.na(x)))
+      stop("all entries of 'x' must be nonnegative and finite")
+    
+    if(!is.integer(x)) {
+        xo <- x
+        x <- round(x)
+        if(any(x > .Machine$integer.max))
+        stop("'x' has entries too large to be integer")
+        if(!identical(TRUE, (ax <- all.equal(xo, x))))
+        warning(gettextf("'x' has been rounded to integer: %d", ax), domain = NA)
+        storage.mode(x) <- "integer"
+    }
+  
+   }else{stop("'x'  must be matrix")}
+  
+
+  
+  STATISTIC <- -sum(lfactorial(x))
+  ##STATISTIC is negative
+  almost.1 <- 1 + 64 * .Machine$double.eps
+  
+  threshold = STATISTIC/almost.1
+  
+  if(missing(streams)) {
+       if(missing(workgroupSize)) 
+           {workgroupSize = c(64,4)
+            streams = cpp_mrg31k3pCreateStreams(prod(workgroupSize))	}	
+        }else {
+              if(!is.matrix(streams)) {
+               warning("streams should be a matrix") }
+    
+    if(prod(workgroupSize) != nrow(streams))
+      warning("number of work items needs to be same as number of streams")
+    # make a deep copy
+    streams = matrix(as.vector(streams), nrow(streams), ncol(streams), FALSE, dimnames(streams))
+  }
+  
+  localSize = pmax(2,c(localSize, 2, 2)[1:2])
+  
+  if(verbose) {
+    cat('local sizes ', toString(localSize), '\nglobal sizes ', toString(workgroupSize),
+        '\n streams ', toString(dim(streams)), '\n')
+  }
+  
+
+  sr0 <- rowSums(x)
+  sc0 <- colSums(x)
+  
+
+  
+  ## we drop all-zero rows and columns
+  x <- x[sr0 > 0, sc0 > 0, drop = FALSE]
+
+  xVcl<-gpuR::vclMatrix(x, type='integer') 
+  
+#  print(class(xVcl))
+
+  
+  
+  if(returnStatistics) {
+    results <- gpuR::vclVector(length=as.integer(B), type=type)
+  } else {
+    results <- gpuR::vclVector(length=as.integer(1), type=type)
+  }
+  
+  
+  PVAL <- NULL
+  
+  counts<-cpp_gpuFisher_test(xVcl, results, threshold, as.integer(B), streams, workgroupSize,localSize)
+  
+  PVAL <- ((1 + counts ) / (as.integer(B) + 1))
+  
+  #if(class(PVAL) == 'try-error') {
+  #  PVAL = counts
+  #}
+  
+
+  
+  if (returnStatistics){
+  
+  theResult = list(pval = PVAL, sim = results, streams=streams)
+    
+  }else {
+    theResult = PVAL
+    attributes(theResult)$streams = streams
+    }
+
+  theResult
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pkg/oldgpuRandom/src/mrg31k3pkernelStringCommon.cpp
@@ -0,0 +1,85 @@
+#include <string>
+
+std::string mrg31k3pString() {
+
+  std::string result = "\n#define CLRNG_ENABLE_SUBSTREAMS\n"
+"#define __CLRNG_DEVICE_API\n"
+
+"#define mrg31k3p_M1 2147483647\n"             /* 2^31 - 1 */
+"#define mrg31k3p_M2 2147462579\n"             /* 2^31 - 21069 */
+
+"#define mrg31k3p_MASK12 511  \n"              /* 2^9 - 1 */
+"#define mrg31k3p_MASK13 16777215  \n"         /* 2^24 - 1 */
+"#define mrg31k3p_MASK2 65535     \n"          /* 2^16 - 1 */
+"#define mrg31k3p_MULT2 21069\n"
+
+"#define MODULAR_NUMBER_TYPE cl_uint\n"
+
+//"typedef double cl_double;\n"
+"typedef float  cl_float;\n"
+"typedef int    cl_int;\n"
+"typedef uint   cl_uint;\n"
+"typedef long   cl_long;\n"
+"typedef ulong  cl_ulong;\n"
+
+
+"#define MRG31K3P_CLH\n"
+
+
+" /******************************************************************************** \n"
+" * Implementation                                                               * \n"
+" ********************************************************************************/ \n"
+
+
+
+/*! @brief Advance the rng one step and returns z such that 1 <= z <= mrg31k3p_M1
+ */
+//"static 
+"cl_uint clrngMrg31k3pNextState(cl_uint *g1, cl_uint *g2) {\n"
+
+"	cl_uint y1, y2;\n"
+
+	// first component
+"	y1 = ((g1[1] & mrg31k3p_MASK12) << 22) + (g1[1] >> 9)\n"
+"		+ ((g1[2] & mrg31k3p_MASK13) << 7) + (g1[2] >> 24);\n"
+
+"	if (y1 >= mrg31k3p_M1)\n"
+"		y1 -= mrg31k3p_M1;\n"
+
+"	y1 += g1[2];\n"
+"	if (y1 >= mrg31k3p_M1)\n"
+"		y1 -= mrg31k3p_M1;\n"
+
+"	g1[2] = g1[1];\n"
+"	g1[1] = g1[0];\n"
+"	g1[0] = y1;\n"
+
+	// second component
+"	y1 = ((g2[0] & mrg31k3p_MASK2) << 15) + (mrg31k3p_MULT2 * (g2[0] >> 16));\n"
+"	if (y1 >= mrg31k3p_M2)\n"
+"		y1 -= mrg31k3p_M2;\n"
+"	y2 = ((g2[2] & mrg31k3p_MASK2) << 15) + (mrg31k3p_MULT2 * (g2[2] >> 16));\n"
+"	if (y2 >= mrg31k3p_M2)\n"
+"		y2 -= mrg31k3p_M2;\n"
+"	y2 += g2[2];\n"
+"	if (y2 >= mrg31k3p_M2)\n"
+"		y2 -= mrg31k3p_M2;\n"
+"	y2 += y1;\n"
+"	if (y2 >= mrg31k3p_M2)\n"
+"		y2 -= mrg31k3p_M2;\n"
+
+"	g2[2] = g2[1];\n"
+"	g2[1] = g2[0];\n"
+"	g2[0] = y2;\n"
+
+"	if (g1[0] <= g2[0]){\n"
+"		return (g1[0] - g2[0] + mrg31k3p_M1);\n"
+//"		result = convert_int(mrg31k3p_M1 - (g2[0] - g1[0]));\n"
+"	} else {\n"
+"		return(g1[0] - g2[0]);\n"
+" }\n"
+"}\n\n";
+return(result);
+}
+
+  
