e5c55e761114a4a056178b22860b2b0930a57648
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-03-10 20:49:04 +0000

    adding datasets
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2387 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/gpuRandom/R/exampleone.R
@@ -0,0 +1,23 @@
+#' Statistics Canada data on leading causes of death, age from 1 to 14
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(exampleone)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Statistics Canada.  Table  13-10-0394-01   Leading causes of death, total population, by age group
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{}{QTL Archive}
+
+
+
+
---
+++ pkg/gpuRandom/R/exampletwo.R
@@ -0,0 +1,20 @@
+#' Statistics Canada data on leading causes of death, all ages
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(exampletwo)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Statistics Canada.  Table  13-10-0394-01   Leading causes of death, total population, by age group
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{}{}
+
