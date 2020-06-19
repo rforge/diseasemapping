390667b27e7cbcc1c114225ff1c93acff659e5cd
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-06-19 18:35:22 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2438 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/inst/include/CL/gemmbatched.hpp
+++ pkg/gpuRandom/inst/include/CL/gemmbatched.hpp
@@ -13,12 +13,12 @@ __kernel void myGEMM2(const int M,
                       __global float* C) {
   
   // Thread identifiers
-  const int localrow = get_local_id(0); // Local row ID 
-  const int localcol = get_local_id(1); // Local col ID 
+  const int row = get_local_id(0); // Local row ID 
+  const int col = get_local_id(1); // Local col ID 
   
   // Identify the row and column of the C matrix to work on
-  const int globalRow = M*get_group_id(0) + localrow; // Row ID of C 
-  const int globalCol = localcol; // Col ID of C 
+ // const int globalRow = M*get_group_id(0) + localrow; // Row ID of C 
+ // const int globalCol = localcol; // Col ID of C 
   
   // Local memory to fit a tile of TS*TS elements of A and B
   __local float Asub[M][K];
@@ -32,10 +32,11 @@ __kernel void myGEMM2(const int M,
   for (int t=0; t<z; t++) {
     
     // Load one batch of A and B into local memory
-    const int batchedRow = M*t + localrow;
-    const int batchedCol = localcol
-    Asub[localcol][localrow] = A[batchedCol*M*z + globalRow];
-    Bsub[localcol][localrow] = B[globalCol*K*z + batchedRow];
+    const int batchedRowA = M*t + row;
+    const int batchedRowB = K*t + row;
+    const int batchedCol = col;
+    Asub[col][row] = A[batchedCol*M*z + batchedRowA];
+    Bsub[col][row] = B[batchedCol*K*z + batchedRowB];
     
     // Synchronise to make sure the tile is loaded
     barrier(CLK_LOCAL_MEM_FENCE);
