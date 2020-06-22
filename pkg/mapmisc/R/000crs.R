99051ada7fe884f29d732d506406ef9bfa9988dc
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-06-22 03:52:57 +0000

    worldMap
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2450 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mapmisc/R/000crs.R
+++ pkg/mapmisc/R/000crs.R
@@ -1,8 +1,8 @@
 # crsLL = CRS("+init=epsg:4326")
-crsLL = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+crsLL = CRS("+proj=longlat +datum=WGS84 +no_defs")
 
 # CRS("+init=epsg:3857") without the nagrids stuff
-crsMerc = CRS("+proj=merc +datum=WGS84 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +ellps=WGS84 +towgs84=0,0,0")
+crsMerc = CRS("+proj=merc +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +datum=WGS84")
 
 #      "+proj=webmerc +ellps=WGS84 +datum=WGS84 +lon_0=0.0 +x_0=0.0 +y_0=0.0")
 #  "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +type=crs"
