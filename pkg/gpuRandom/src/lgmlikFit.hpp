9245b159b04818bb1d2af1ae2f8705872324f69d
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-04-04 15:19:46 +0000

    likfitGpu.cpp error
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2664 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/src/lgmlikFit.hpp
+++ pkg/gpuRandom/src/lgmlikFit.hpp
@@ -1,6 +1,19 @@
 
 #include "viennacl/vector_proxy.hpp"
 #include "viennacl/matrix_proxy.hpp"
+#include "viennacl/linalg/sum.hpp"
+#include "viennacl/ocl/backend.hpp"
+#include <Rcpp.h>
+
+
+extern "C" void Rtemme_gamma(double *nu, double * g_1pnu, double * g_1mnu, double *g1, double *g2);
+
+//template<typename T> double luT(viennacl::matrix<T> &vclX, viennacl::vector_base<T> &vclD);
+
+template <typename T> T maternClEpsilon();
+
+
+
 
 
 template<typename T> 
