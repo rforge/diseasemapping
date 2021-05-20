8708017d70267dd3d6875b0bb60e500c7890c43d
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-05-20 14:01:00 +0000

    some debugging
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2724 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/brnd.sh
+++ pkg/brnd.sh
@@ -1,4 +1,4 @@
 R -e "Rcpp::compileAttributes('gpuRandom')"
-R -e "devtools::document('gpuRandom')"
+#R -e "devtools::document('gpuRandom')"
 R CMD build --no-build-vignettes gpuRandom
 R CMD INSTALL gpuRandom_0.1.tar.gz
