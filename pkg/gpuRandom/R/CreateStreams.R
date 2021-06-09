39cb2a7aa72f0b93c885ecbfe07932eb823ff9bb
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-06-09 08:48:37 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2737 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/R/CreateStreams.R
+++ pkg/gpuRandom/R/CreateStreams.R
@@ -9,9 +9,7 @@
 
 CreateStreams = function(numWorkItems) {
 
-  result = matrix(0, nrow=numWorkItems, ncol=18)
-  
- # mode(result) <- "integer"
+  result = matrix(as.integer(0), nrow=numWorkItems, ncol=18)
   
   colnames(result) = c("current.g1.1", "current.g1.2", "current.g1.3", "current.g2.1", "current.g2.2", "current.g2.3",
                        "initial.g1.1", "initial.g1.2", "initial.g1.3", "initial.g2.1", "initial.g2.2", "initial.g2.3",
@@ -22,6 +20,5 @@ CreateStreams = function(numWorkItems) {
 
 
 }
-
 
 
