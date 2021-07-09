13d7260d02da0eb1d7d77f875504812b4cb30ede
ruoyong <ruoyong@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-07-09 07:34:33 +0000

    workgroupSize
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diseasemapping@2758 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gpuRandom/R/rnorm.R
+++ pkg/gpuRandom/R/rnorm.R
@@ -8,7 +8,7 @@
 rnorm = function(
   n, 
   streams, 
-  workgroupSize,
+  Nglobal,
   type=c("double","float")) {
   
   
@@ -25,8 +25,8 @@ rnorm = function(
   #x<-matrix(0,nrow=n[1],ncol=n[2])
   
   if(missing(streams)) {
-    if(missing(workgroupSize)) {
-      workgroupSize = c(64,8)
+    if(missing(Nglobal)) {
+      Nglobal = c(64,8)
       seedR = as.integer(as.integer(2^31-1)*(2*stats::runif(6) - 1) ) 
       seed <- gpuR::vclVector(seedR, type="integer")  
       streams<-vclMatrix(0L, nrow=512, ncol=18, type="integer")
@@ -34,12 +34,12 @@ rnorm = function(
     }else{
       seedR = as.integer(as.integer(2^31-1)*(2*stats::runif(6) - 1) ) 
       seed <- gpuR::vclVector(seedR, type="integer")  
-      streams<-vclMatrix(0L, nrow=prod(workgroupSize), ncol=18, type="integer")
+      streams<-vclMatrix(0L, nrow=prod(Nglobal), ncol=18, type="integer")
       CreateStreamsGpuBackend(seed, streams, keepInitial=1)
     }
-  }else if(missing(workgroupSize)){
+  }else if(missing(Nglobal)){
     stop("number of work items needs to be same as number of streams")
-  }else if(prod(workgroupSize) != nrow(streams)){
+  }else if(prod(Nglobal) != nrow(streams)){
     warning("number of work items needs to be same as number of streams")
   }
   
@@ -49,7 +49,7 @@ rnorm = function(
   xVcl<-gpuR::vclMatrix(0, nrow=n[1], ncol=n[2], type=type[1])     
   
   
-  gpuRnBackend(xVcl,streams,workgroupSize,"normal") 
+  gpuRnBackend(xVcl,streams,Nglobal,"normal") 
   
   invisible(streams)
   
@@ -67,27 +67,27 @@ rnorm = function(
 
 
 # if(missing(streams)) {
-#   if(missing(workgroupSize)) {
-#     workgroupSize = c(64,8)
-#     result = matrix(0L, nrow=prod(workgroupSize), ncol=18)
+#   if(missing(Nglobal)) {
+#     Nglobal = c(64,8)
+#     result = matrix(0L, nrow=prod(Nglobal), ncol=18)
 #     colnames(result) = c("current.g1.1", "current.g1.2", "current.g1.3", "current.g2.1", "current.g2.2", "current.g2.3",
 #                          "initial.g1.1", "initial.g1.2", "initial.g1.3", "initial.g2.1", "initial.g2.2", "initial.g2.3",
 #                          "substream.g1.1", "substream.g1.2", "substream.g1.3", "substream.g2.1", "substream.g2.2", "substream.g2.3")
 #     streams = gpuR::vclMatrix(cpp_mrg31k3pCreateStreams(result))
 #   }else{
-#     result = matrix(0L, nrow=prod(workgroupSize), ncol=18)
+#     result = matrix(0L, nrow=prod(Nglobal), ncol=18)
 #     colnames(result) = c("current.g1.1", "current.g1.2", "current.g1.3", "current.g2.1", "current.g2.2", "current.g2.3",
 #                          "initial.g1.1", "initial.g1.2", "initial.g1.3", "initial.g2.1", "initial.g2.2", "initial.g2.3",
 #                          "substream.g1.1", "substream.g1.2", "substream.g1.3", "substream.g2.1", "substream.g2.2", "substream.g2.3")
 #     streams = gpuR::vclMatrix(cpp_mrg31k3pCreateStreams(result))
 #   }
-# }else if(missing(workgroupSize)){
+# }else if(missing(Nglobal)){
 #   stop("number of work items needs to be same as number of streams")
 # }else{
 #   if(!isS4(streams)) {
 #     warning("streams should be a S4 matrix") }
 #   
-#   if(prod(workgroupSize) != nrow(streams))
+#   if(prod(Nglobal) != nrow(streams))
 #     warning("number of work items needs to be same as number of streams")
 #   # make a deep copy
 #   # streams = gpuR::vclMatrix(as.matrix(streams), nrow(streams), ncol(streams), FALSE, dimnames(streams))
--- pkg/gpuRandom/R/runif.R
+++ pkg/gpuRandom/R/runif.R
@@ -7,7 +7,7 @@
 runif = function(
   n, 
   streams, 
-  workgroupSize,
+  Nglobal,
   type=c("double","float")) {
   
   
@@ -22,8 +22,8 @@ runif = function(
   }
   
   if(missing(streams)) {
-    if(missing(workgroupSize)) {
-      workgroupSize = c(64,8)
+    if(missing(Nglobal)) {
+      Nglobal = c(64,8)
       seedR = as.integer(as.integer(2^31-1)*(2*stats::runif(6) - 1) ) 
       seed <- gpuR::vclVector(seedR, type="integer")  
       streams<-vclMatrix(0L, nrow=512, ncol=18, type="integer")
@@ -31,12 +31,12 @@ runif = function(
     }else{
       seedR = as.integer(as.integer(2^31-1)*(2*stats::runif(6) - 1) ) 
       seed <- gpuR::vclVector(seedR, type="integer")  
-      streams<-vclMatrix(0L, nrow=prod(workgroupSize), ncol=18, type="integer")
+      streams<-vclMatrix(0L, nrow=prod(Nglobal), ncol=18, type="integer")
       CreateStreamsGpuBackend(seed, streams, keepInitial=1)
     }
-  }else if(missing(workgroupSize)){
+  }else if(missing(Nglobal)){
     stop("number of work items needs to be same as number of streams")
-  }else if(prod(workgroupSize) != nrow(streams)){
+  }else if(prod(Nglobal) != nrow(streams)){
       warning("number of work items needs to be same as number of streams")
   }
   
@@ -45,7 +45,7 @@ runif = function(
   xVcl<-gpuR::vclMatrix(0, nrow=n[1], ncol=n[2], type=type[1])    
   
   
-  gpuRnBackend(xVcl,streams,workgroupSize,"uniform") 
+  gpuRnBackend(xVcl,streams,Nglobal,"uniform") 
   
   invisible(streams)
   
@@ -63,27 +63,27 @@ runif = function(
 
 
 # if(missing(streams)) {
-#   if(missing(workgroupSize)) {
-#     workgroupSize = c(64,8)
-#     result = matrix(0, nrow=prod(workgroupSize), ncol=18)
+#   if(missing(Nglobal)) {
+#     Nglobal = c(64,8)
+#     result = matrix(0, nrow=prod(Nglobal), ncol=18)
 #     colnames(result) = c("current.g1.1", "current.g1.2", "current.g1.3", "current.g2.1", "current.g2.2", "current.g2.3",
 #                          "initial.g1.1", "initial.g1.2", "initial.g1.3", "initial.g2.1", "initial.g2.2", "initial.g2.3",
 #                          "substream.g1.1", "substream.g1.2", "substream.g1.3", "substream.g2.1", "substream.g2.2", "substream.g2.3")
 #     streams = gpuR::vclMatrix(cpp_mrg31k3pCreateStreams(result))
 #   }else{
-#     result = matrix(0, nrow=prod(workgroupSize), ncol=18)
+#     result = matrix(0, nrow=prod(Nglobal), ncol=18)
 #     colnames(result) = c("current.g1.1", "current.g1.2", "current.g1.3", "current.g2.1", "current.g2.2", "current.g2.3",
 #                          "initial.g1.1", "initial.g1.2", "initial.g1.3", "initial.g2.1", "initial.g2.2", "initial.g2.3",
 #                          "substream.g1.1", "substream.g1.2", "substream.g1.3", "substream.g2.1", "substream.g2.2", "substream.g2.3")
 #     streams = gpuR::vclMatrix(cpp_mrg31k3pCreateStreams(result))
 #   }
-# }else if(missing(workgroupSize)){
+# }else if(missing(Nglobal)){
 #   stop("number of work items needs to be same as number of streams")
 # }else{
 #   if(!isS4(streams)) {
 #     warning("streams should be a S4 matrix") }
 #   
-#   if(prod(workgroupSize) != nrow(streams))
+#   if(prod(Nglobal) != nrow(streams))
 #     warning("number of work items needs to be same as number of streams")
 #   # make a deep copy
 #   # streams = gpuR::vclMatrix(as.matrix(streams), nrow(streams), ncol(streams), FALSE, dimnames(streams))
